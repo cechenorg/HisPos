bc9f1d057ed8109781594f00e809e53a4c9b365d
xilian <l1o2u3i45s1@gmail.com>
2018-03-01 19:47:32 +0800

    OK

--- InventoryManagement/InventoryManagementView.xaml.cs
+++ InventoryManagement/InventoryManagementView.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using His_Pos.Class;
+using His_Pos.Properties;
+using His_Pos.Service;
 
 namespace His_Pos.InventoryManagement
 {
@@ -20,9 +26,55 @@ namespace His_Pos.InventoryManagement
     /// </summary>
     public partial class InventoryManagementView : UserControl
     {
+        private readonly ObservableCollection<Inventory> _inventoryList = new ObservableCollection<Inventory>();
         public InventoryManagementView()
         {
             InitializeComponent();
         }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            DataBinding(start.Text,end.Text, Name.Text,ID.Text);
+        }
+
+        private void DataBinding(string start,string end,string name,string id) {
+            var conn = new DbConnection(Settings.Default.SQL_global);
+            DateTimeExtensions dateTimeExtensions = new DateTimeExtensions();
+            var parameters = new List<SqlParameter>();
+            if (start != string.Empty)
+                    parameters.Add(new SqlParameter("SDATE", dateTimeExtensions.UsToTaiwan(start)));
+            else
+                parameters.Add(new SqlParameter("SDATE", DBNull.Value));
+            if (end != string.Empty)
+                parameters.Add(new SqlParameter("EDATE", dateTimeExtensions.UsToTaiwan(end)));
+            else
+                parameters.Add(new SqlParameter("EDATE", DBNull.Value));
+            if (name != string.Empty)
+                parameters.Add(new SqlParameter("NAME", name));
+            else
+                parameters.Add(new SqlParameter("NAME", DBNull.Value));
+            if (id != string.Empty)
+                parameters.Add(new SqlParameter("ID", id));
+            else
+                parameters.Add(new SqlParameter("ID", DBNull.Value));
+
+            var table = conn.ExecuteProc("[HIS_POS_DB].[GET].[INVENTORYDATA]", parameters);
+            foreach (DataRow row in table.Rows)
+            {
+                var inventory = new Inventory();
+                SetValue(ref inventory, row);
+                _inventoryList.Add(inventory);
+            }
+            DataGrid.ItemsSource = _inventoryList;
+        }
+        private void SetValue(ref Inventory inventory,DataRow row)
+        {
+            inventory.ProductId = row["PRO_ID"].ToString();
+            inventory.ProductName = row["PRO_NAME"].ToString();
+            inventory.ProductSafeAmount = row["PRO_SAFEQTY"].ToString();
+            inventory.ProductTotalAmount = row["PRO_INVENTORY"].ToString();
+            inventory.ProductCurrentTime = row["PURCHASE_DATE"].ToString();
+        }
+
     }
 }
