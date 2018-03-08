f12fcd23f556396bc74c9563ee503bb1edf0feb7
世彬 洪 <touycvbn95147@gmail.com>
2018-03-08 18:00:53 +0800

    new detail style

---
+++ Class/Product/CusOrderOverview.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace His_Pos.Class.Product
+{
+    public class CusOrderOverview
+    {
+        public CusOrderOverview(string date, string amount, string profit)
+        {
+            Date = date;
+            Amount = amount;
+            Profit = profit;
+        }
+
+        public string Date { get; }
+        public string Amount { get; }
+        public string Profit { get; }
+    }
+}
--- InventoryManagement/OtcDetail.xaml.cs
+++ InventoryManagement/OtcDetail.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using His_Pos.Class.Product;
+using LiveCharts;
+using LiveCharts.Definitions.Series;
+using LiveCharts.Wpf;
 
 namespace His_Pos.InventoryManagement
 {
@@ -20,21 +24,98 @@ namespace His_Pos.InventoryManagement
     /// </summary>
     public partial class OtcDetail : Window
     {
+        public SeriesCollection InventoryCollection { get; set; }
+        public string[] Days { get; set; }
+        public Func<double, string> DataFormatter { get; set; }
+        public ObservableCollection<CusOrderOverview> CusOrderOverviewCollection;
+
         private Otc otc;
+
         public OtcDetail(Otc o)
         {
             InitializeComponent();
 
             otc = o;
+            
             UpdateUi();
+            CheckAuth();
+
+            DataContext = this;
+        }
+
+        private void CheckAuth()
+        {
+            
+        }
+
+        private void UpdateChart()
+        {
+            InventoryCollection = new SeriesCollection();
+            InventoryCollection.Add(GetInventoryLineSeries());
+            InventoryCollection.Add(GetSaveAmountLineSeries());
+            
+            DataFormatter = value => value.ToString();
+
+            AddDays();
+        }
+
+        private LineSeries GetInventoryLineSeries()
+        {
+            return new LineSeries
+            {
+                Title = "庫存",
+                Values = new ChartValues<double> { 4, 5, 5, 5, 4 },
+                PointGeometrySize = 10,
+                LineSmoothness = 0,
+                DataLabels = true
+            };
+        }
+
+        private LineSeries GetSaveAmountLineSeries()
+        {
+            ChartValues<double> chartValues = new ChartValues<double>();
+
+            for (int x = 0; x < 30; x++)
+            {
+                chartValues.Add( double.Parse(otc.SafeAmount) );
+            }
+            
+            return new LineSeries
+            {
+                Title = "安全量",
+                Values = chartValues,
+                PointGeometry = null
+            };
+        }
+
+        private void AddDays()
+        {
+            DateTime today = DateTime.Today.Date;
+
+            Days = new string[30];
+            for (int x = 0; x < 30; x++)
+            {
+                Days[x] = today.AddDays(x - 29).Date.ToString("dd/MM");
+            }
         }
 
         private void UpdateUi()
         {
             if (otc is null) return;
 
-            ProductName.Content = otc.Name;
-            ProductId.Content = otc.Id;
+            OtcName.Content = otc.Name;
+            OtcId.Content = otc.Id;
+
+            OtcPrice.Text = otc.Price.ToString();
+            OtcInventory.Text = otc.Inventory.ToString();
+            OtcCost.Text = otc.Cost.ToString();
+            OtcSaveAmount.Text = otc.SafeAmount.ToString();
+            OtcManufactory.Text = otc.ManufactoryName;
+
+            CusOrderOverviewCollection = OTCDb.GetOtcCusOrderOverviewByID(otc.Id);
+            OtcCusOrder.ItemsSource = CusOrderOverviewCollection;
+
+            UpdateChart();
         }
     }
 }
