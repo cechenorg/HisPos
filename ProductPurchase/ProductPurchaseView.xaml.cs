642a1f63953fee57f887eeaf2da7f029bbd9f8ec
xilian <user@MSI>
2018-04-02 09:52:43 +0800

    OK

--- ProductPurchase/ProductPurchaseView.xaml.cs
+++ ProductPurchase/ProductPurchaseView.xaml.cs
@@ -146,19 +146,17 @@ namespace His_Pos.ProductPurchase
         {
             var productAuto = sender as AutoCompleteBox;
             ProductAutoCompleteCollection.Clear();
-
-            var tmp = MainWindow.MedicineDataTable.Select("PRO_ID Like '%" + productAuto.Text + "%' OR PRO_NAME Like '%" + productAuto.Text + "%'");
-            foreach (var d in tmp.Take(50))
-            {
-                ProductAutoCompleteCollection.Add(new Medicine(d, DataSource.MEDICINE));
-            }
-
             var tmp1 = MainWindow.OtcDataTable.Select("PRO_ID Like '%" + productAuto.Text + "%' OR PRO_NAME Like '%" + productAuto.Text + "%'");
             foreach (var d in tmp1.Take(50))
             {
                 ProductAutoCompleteCollection.Add(new Otc(d, DataSource.OTC));
             }
-
+            var tmp = MainWindow.MedicineDataTable.Select("PRO_ID Like '%" + productAuto.Text + "%' OR PRO_NAME Like '%" + productAuto.Text + "%'");
+            foreach (var d in tmp.Take(50))
+            {
+                ProductAutoCompleteCollection.Add(new Medicine(d, DataSource.MEDICINE));
+            }
+            
             productAuto.ItemsSource = ProductAutoCompleteCollection;
             productAuto.PopulateComplete();
         }
