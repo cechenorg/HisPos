4c579c8624a2f19f81dc3c7aa043b1b4a8d731a7
xilian <user@MSI>
2018-04-16 11:56:12 +0800

    OK

--- ProductPurchase/ProductPurchaseView.xaml.cs
+++ ProductPurchase/ProductPurchaseView.xaml.cs
@@ -151,6 +151,7 @@ namespace His_Pos.ProductPurchase
             OrderCategory.Text = storeOrder.Category;
             Total.Content = storeOrder.TotalPrice;
             ManufactoryAuto.Text = (storeOrder.Manufactory.Name is null)? "": storeOrder.Manufactory.Name;
+            ButtonNewProduct.IsEnabled = (storeOrder.Manufactory.Name is null) ? false :true;
             Phone.Content = (storeOrder.Manufactory.Telphone is null)? "": storeOrder.Manufactory.Telphone;
 
             if (storeOrder.Products is null)
@@ -159,7 +160,6 @@ namespace His_Pos.ProductPurchase
             storeOrderData = storeOrder;
             StoreOrderDetail.ItemsSource = storeOrderData.Products;
             TotalAmount.Content = storeOrder.Products.Count.ToString();
-            
             IsChanged = false;
             IsFirst = false;
         }
@@ -297,6 +297,8 @@ namespace His_Pos.ProductPurchase
             AutoCompleteBox autoCompleteBox = sender as AutoCompleteBox;
             if (autoCompleteBox is null || autoCompleteBox.SelectedItem is null) return;
             Phone.Content = ((Manufactory)autoCompleteBox.SelectedItem).Telphone;
+            ButtonNewProduct.IsEnabled = (ManufactoryAuto.Text == string.Empty) ? false : true;
+            storeOrderData.Manufactory = ((Manufactory)autoCompleteBox.SelectedItem);
         }
 
         private void AutoCompleteBox_DropDownClosed(object sender, RoutedPropertyChangedEventArgs<bool> e)
