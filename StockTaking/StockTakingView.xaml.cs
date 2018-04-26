057b6c90b21708bdc3cd316c9b17a5fc70ba1b92
世彬 洪 <touycvbn95147@gmail.com>
2018-04-26 15:30:14 +0800

    ui update

--- StockTaking/StockTakingView.xaml.cs
+++ StockTaking/StockTakingView.xaml.cs
@@ -25,6 +25,7 @@ namespace His_Pos.StockTaking
         public StockTakingView()
         {
             InitializeComponent();
+            UpdateUi();
         }
 
         private void UpdateUi()
@@ -34,7 +35,9 @@ namespace His_Pos.StockTaking
                 case StockTakingStatus.ADDPRODUCTS:
                     ViewGrid.RowDefinitions[2].Height = new GridLength(15);
                     ViewGrid.RowDefinitions[3].Height = new GridLength(150);
-                    Print.IsEnabled = false;
+                    ViewGrid.RowDefinitions[5].Height = new GridLength(50);
+                    ViewGrid.RowDefinitions[6].Height = new GridLength(0);
+                    ViewGrid.RowDefinitions[7].Height = new GridLength(0);
                     AddProductsEllipse.Fill = Brushes.GreenYellow;
                     PrintLine.Stroke = Brushes.LightSlateGray;
                     PrintLine.StrokeDashArray = new DoubleCollection() { 4 };
@@ -45,24 +48,30 @@ namespace His_Pos.StockTaking
                     CompleteLine.Stroke = Brushes.LightSlateGray;
                     CompleteLine.StrokeDashArray = new DoubleCollection() { 4 };
                     CompleteEllipse.Fill = Brushes.LightSlateGray;
+                    FinishedAddProduct.Visibility = Visibility.Visible;
+                    Print.Visibility = Visibility.Collapsed;
                     break;
                 case StockTakingStatus.PRINT:
                     ViewGrid.RowDefinitions[2].Height = new GridLength(0);
                     ViewGrid.RowDefinitions[3].Height = new GridLength(0);
-                    Print.IsEnabled = true;
                     AddProductsEllipse.Fill = Brushes.DeepSkyBlue;
                     PrintLine.Stroke = Brushes.DeepSkyBlue;
                     PrintLine.StrokeDashArray = new DoubleCollection() { 300 };
                     PrintEllipse.Fill = Brushes.GreenYellow;
+                    FinishedAddProduct.Visibility = Visibility.Collapsed;
+                    Print.Visibility = Visibility.Visible;
                     break;
                 case StockTakingStatus.INPUTRESULT:
-                    Print.IsEnabled = false;
+                    ViewGrid.RowDefinitions[5].Height = new GridLength(0);
+                    ViewGrid.RowDefinitions[6].Height = new GridLength(50);
                     PrintEllipse.Fill = Brushes.DeepSkyBlue;
                     InputLine.Stroke = Brushes.DeepSkyBlue;
                     InputLine.StrokeDashArray = new DoubleCollection() { 300 };
                     InputEllipse.Fill = Brushes.GreenYellow;
                     break;
                 case StockTakingStatus.COMPLETE:
+                    ViewGrid.RowDefinitions[6].Height = new GridLength(0);
+                    ViewGrid.RowDefinitions[7].Height = new GridLength(50);
                     InputEllipse.Fill = Brushes.DeepSkyBlue;
                     CompleteLine.Stroke = Brushes.DeepSkyBlue;
                     CompleteLine.StrokeDashArray = new DoubleCollection() { 300 };
@@ -73,13 +82,15 @@ namespace His_Pos.StockTaking
 
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void NextStatus_Click(object sender, RoutedEventArgs e)
         {
-            if (stockTakingStatus == StockTakingStatus.COMPLETE)
-                stockTakingStatus = StockTakingStatus.ADDPRODUCTS;
-            else
-                stockTakingStatus++;
+            stockTakingStatus++;
+            UpdateUi();
+        }
 
+        private void Complete_Click(object sender, RoutedEventArgs e)
+        {
+            stockTakingStatus = StockTakingStatus.ADDPRODUCTS;
             UpdateUi();
         }
     }
