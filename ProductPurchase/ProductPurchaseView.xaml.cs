743c2e6e4b2a1cd22de5ce7947b2cb4e572814b7
世彬 洪 <touycvbn95147@gmail.com>
2018-05-10 17:46:58 +0800

    froze col, batch enable

--- ProductPurchase/ProductPurchaseView.xaml.cs
+++ ProductPurchase/ProductPurchaseView.xaml.cs
@@ -857,18 +857,4 @@ namespace His_Pos.ProductPurchase
             return null;
         }
     }
-
-    public class BatchNamberIsEnableConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value is ProductPurchaseMedicine) return true;
-            return false;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return "";
-        }
-    }
 }
