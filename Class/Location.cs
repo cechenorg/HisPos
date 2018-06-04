2d2865c7348025907a1c8425b56dd7d511cd279c
xilian <user@MSI>
2018-06-04 15:27:26 +0800

    OK

---
+++ Class/Location.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace His_Pos.Class
+{
+    public class Location
+    {
+        public string id;
+        public string name;
+        public double pathX;
+        public double pathY;
+        public double width;
+        public double height;
+        public ObservableCollection<LocationDetail> locationDetailCollection = new ObservableCollection<LocationDetail>();
+    }
+}
