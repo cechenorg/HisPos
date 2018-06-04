9863c0b0725f22e9324367944d48dacfed14c80a
世彬 洪 <touycvbn95147@gmail.com>
2018-06-04 12:02:20 +0800

    draw location ok

--- H4_BASIC_MANAGE/LocationManage/LocationManageView.xaml.cs
+++ H4_BASIC_MANAGE/LocationManage/LocationManageView.xaml.cs
@@ -24,5 +24,28 @@ namespace His_Pos.LocationManage
         {
             InitializeComponent();
         }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            ContentControl c = new ContentControl();
+            c.Template = (ControlTemplate)FindResource("DesignerItemTemplate");
+
+            Grid g = new Grid();
+
+            Rectangle r = new Rectangle();
+            r.Fill = Brushes.DarkRed;
+            r.IsHitTestVisible = false;
+
+
+            g.Children.Add(r);
+
+            c.Height = 50;
+            c.Width = 50;
+            c.Content = g;
+
+            LocationCanvus.Children.Add(c);
+            Canvas.SetTop(c, 200);
+            Canvas.SetLeft(c, 75);
+        }
     }
 }
