7ec61b7fc48a1097ac63c16b4f20cce816e5b197
世彬 洪 <touycvbn95147@gmail.com>
2018-06-21 19:51:40 +0800

    get data ok

---
+++ Class/Manufactory/ManufactoryPrincipal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace His_Pos.Class.Manufactory
+{
+    public class ManufactoryPrincipal
+    {
+        public ManufactoryPrincipal(DataRow row)
+        {
+            Id = row["MAN_ID"].ToString();
+            Name = row["MAN_NAME"].ToString();
+            NickName = row["MAN_NICKNAME"].ToString();
+            Telphone = row["MAN_TEL"].ToString();
+            Fax = row["MAN_FAX"].ToString();
+            Email = row["MAN_EMAIL"].ToString();
+            PayType = "";
+            ResponsibleDepartment = row["MAN_RESPONSIBLEDEP"].ToString();
+        }
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string NickName { get; set; }
+        public string Telphone { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+        public string PayType { get; set; }
+        public string ResponsibleDepartment { get; set; }
+    }
+}
--- H4_BASIC_MANAGE/ManufactoryManage/ManufactoryManageView.xaml.cs
+++ H4_BASIC_MANAGE/ManufactoryManage/ManufactoryManageView.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using His_Pos.Class.Manufactory;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +21,58 @@ namespace His_Pos.ManufactoryManage
     /// <summary>
     /// ManufactoryManageView.xaml 的互動邏輯
     /// </summary>
-    public partial class ManufactoryManageView : UserControl
+    public partial class ManufactoryManageView : UserControl, INotifyPropertyChanged
     {
+        public ManageManufactory currentManufactory;
+        public ManageManufactory CurrentManufactory
+        {
+            get { return currentManufactory; }
+            set
+            {
+                currentManufactory = value;
+                NotifyPropertyChanged("CurrentManufactory");
+            }
+        }
+        public ObservableCollection<ManageManufactory> ManageManufactories { get; set; }
+
         public ManufactoryManageView()
         {
             InitializeComponent();
+
+            InitManufactory();
+
+
+            DataContext = this;
+        }
+
+        private void InitManufactory()
+        {
+            ManageManufactories = ManufactoryDb.GetManageManufactory();
+            ManageManufactoryDataGrid.SelectedIndex = 0;
+        }
+
+        private void Manufactory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            CurrentManufactory = (sender as DataGrid).SelectedItem as ManageManufactory;
+
+            if (CurrentManufactory.ManufactoryPrincipals.Count > 0)
+                PrincipalDataGrid.SelectedIndex = 0;
+            else
+                PrincipalDetail.DataContext = null;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        private void Principals_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            PrincipalDetail.DataContext = (sender as DataGrid).SelectedItem;
         }
     }
 }
