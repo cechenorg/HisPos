7a775b94d2b20536b529c0139385ca1ed1dd28da
世彬 洪 <touycvbn95147@gmail.com>
2018-07-19 12:14:22 +0800

    leave ok

--- H5_ATTEND/WorkScheduleManage/Leave/LeaveWindow.xaml.cs
+++ H5_ATTEND/WorkScheduleManage/Leave/LeaveWindow.xaml.cs
@@ -55,27 +55,26 @@ namespace His_Pos.H5_ATTEND.WorkScheduleManage.Leave
                 return;
             }
 
-            if (EndDate.Text.Equals(""))
-                EndDate.SelectedDate = StartDate.SelectedDate;
-
-            DateTime startDateTime = (DateTime)StartDate.SelectedDate;
-
-            if(StartTime.SelectedTime != null)
+            if ((bool)LHour.IsChecked)
             {
+                DateTime startDateTime = (DateTime)StartDate.SelectedDate;
                 startDateTime = startDateTime.AddHours(((DateTime)StartTime.SelectedTime).Hour);
                 startDateTime = startDateTime.AddMinutes(((DateTime)StartTime.SelectedTime).Minute);
-            }
-            
-            DateTime endDateTime = (DateTime)EndDate.SelectedDate;
 
-            if (EndTime.SelectedTime != null)
-            {
+                DateTime endDateTime = (DateTime)StartDate.SelectedDate;
                 endDateTime = endDateTime.AddHours(((DateTime)EndTime.SelectedTime).Hour);
                 endDateTime = endDateTime.AddMinutes(((DateTime)EndTime.SelectedTime).Minute);
+
+                LeaveDb.AddNewLeave((UserName.SelectedItem as UserIconData).Id, (DayOffType.SelectedItem as Class.Leave.Leave).Id, startDateTime, endDateTime, Note.Text);
             }
+            else
+            {
+                if (EndDate.Text.Equals(""))
+                    EndDate.SelectedDate = StartDate.SelectedDate;
 
-            LeaveDb.AddNewLeave((UserName.SelectedItem as UserIconData).Id, (DayOffType.SelectedItem as Class.Leave.Leave).Id, startDateTime, endDateTime, Note.Text);
-
+                LeaveDb.AddNewLeave((UserName.SelectedItem as UserIconData).Id, (DayOffType.SelectedItem as Class.Leave.Leave).Id, (DateTime)StartDate.SelectedDate, (DateTime)EndDate.SelectedDate, Note.Text);
+            }
+            
             LeaveComplete = true;
             Close();
         }
@@ -87,17 +86,58 @@ namespace His_Pos.H5_ATTEND.WorkScheduleManage.Leave
             if (DayOffType.SelectedItem is null)
                 error += "假別未填寫!\n";
 
-            if (StartDate.Text.Equals(""))
-                error += "日期未填寫!\n";
-            else if(StartDate.SelectedDate < DateTime.Today || EndDate.SelectedDate < DateTime.Today)
-                error += "所選日期已過!\n";
-            else if (EndDate.SelectedDate < StartDate.SelectedDate)
-                error += "結束日期小於起始日期!\n";
-
-            if(Note.Text.Equals(""))
+            if (Note.Text.Equals(""))
                 error += "原因未填寫!\n";
 
+            if (StartDate.Text.Equals(""))
+                error += "日期未填寫!\n";
+            else
+            {
+                if ((bool)LHour.IsChecked)
+                {
+                    if (StartDate.SelectedDate < DateTime.Today)
+                        error += "所選日期已過!\n";
+
+                    if (StartTime.SelectedTime is null)
+                        error += "起始時間未填寫!\n";
+
+                    if (EndTime.SelectedTime is null)
+                        error += "結束時間未填寫!\n";
+
+                    if(EndTime.SelectedTime < StartTime.SelectedTime)
+                        error += "結束時間小於起始時間!\n";
+                }
+                else
+                {
+                    if (StartDate.SelectedDate < DateTime.Today || EndDate.SelectedDate < DateTime.Today)
+                        error += "所選日期已過!\n";
+                    else if (EndDate.SelectedDate < StartDate.SelectedDate)
+                        error += "結束日期小於起始日期!\n";
+                }
+            }
+            
+            
+
+
             return error;
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton radioButton = sender as RadioButton;
+
+            if (radioButton is null || LeaveDays is null || LeaveHours is null) return;
+
+            if( radioButton.Tag.ToString().Equals("0") )
+            {
+                LeaveDays.Visibility = Visibility.Visible;
+                LeaveHours.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                LeaveDays.Visibility = Visibility.Collapsed;
+                LeaveHours.Visibility = Visibility.Visible;
+            }
         }
     }
 }
