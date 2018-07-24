0ed91243361f40d4740af8045b6825103a5e8155
Frankemg <fk035372150@gmail.com>
2018-07-24 17:22:49 +0800

    update medbagLocation

---
+++ H4_BASIC_MANAGE/MedBagManage/RDLLocationResizeThumb.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace His_Pos.H4_BASIC_MANAGE.MedBagManage
+{
+    public class RDLLocationResizeThumb : Thumb
+    {
+        public RDLLocationResizeThumb()
+        {
+            DragDelta += new DragDeltaEventHandler(this.ResizeThumb_DragDelta);
+        }
+
+        private void ResizeThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            Control designerItem = this.DataContext as Control;
+
+            if (designerItem != null)
+            {
+                double deltaVertical, deltaHorizontal;
+
+                switch (VerticalAlignment)
+                {
+                    case VerticalAlignment.Bottom:
+                        deltaVertical = Math.Min(-e.VerticalChange, designerItem.ActualHeight - designerItem.MinHeight);
+                        designerItem.Height -= deltaVertical;
+                        break;
+                    case VerticalAlignment.Top:
+                        deltaVertical = Math.Min(e.VerticalChange, designerItem.ActualHeight - designerItem.MinHeight);
+                        Canvas.SetTop(designerItem, Canvas.GetTop(designerItem) + deltaVertical);
+                        designerItem.Height -= deltaVertical;
+                        break;
+                    default:
+                        break;
+                }
+
+                switch (HorizontalAlignment)
+                {
+                    case HorizontalAlignment.Left:
+                        deltaHorizontal = Math.Min(e.HorizontalChange, designerItem.ActualWidth - designerItem.MinWidth);
+                        Canvas.SetLeft(designerItem, Canvas.GetLeft(designerItem) + deltaHorizontal);
+                        designerItem.Width -= deltaHorizontal;
+                        break;
+                    case HorizontalAlignment.Right:
+                        deltaHorizontal = Math.Min(-e.HorizontalChange, designerItem.ActualWidth - designerItem.MinWidth);
+                        designerItem.Width -= deltaHorizontal;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            designerItem.Height = designerItem.Height < 10 ? 10 : designerItem.Height;
+            designerItem.Width = designerItem.Width < 10 ? 10 : designerItem.Width;
+            e.Handled = true;
+        }
+    }
+}
