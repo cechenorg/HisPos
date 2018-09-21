134394f1c199a2e017818d73e1943b5f2f69d151
世彬 洪 <touycvbn95147@gmail.com>
2018-09-21 12:02:26 +0800

    return class ok

---
+++ Class/Product/ProductReturnOTC.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using His_Pos.Interface;
+
+namespace His_Pos.Class.Product
+{
+    public class ProductReturnOTC : AbstractClass.Product, IProductReturn, INotifyPropertyChanged, ICloneable
+    {
+        public ProductReturnOTC(DataRow row) : base(row)
+        {
+            Stock = new InStock(row);
+
+            Note = row[""].ToString();
+            ReturnAmount = Double.Parse(row[""].ToString());
+            BatchNumber = row[""].ToString();
+            BatchLimit = Double.Parse(row[""].ToString());
+            ReturnPrice = Double.Parse(row[""].ToString());
+            ReturnTotalPrice = Double.Parse(row[""].ToString());
+        }
+
+        public string Note { get; set; }
+        public InStock Stock { get; set; }
+        public double ReturnAmount { get; set; }
+        public string BatchNumber { get; set; }
+        public double BatchLimit { get; set; }
+        public double ReturnPrice { get; set; }
+        public double ReturnTotalPrice { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
