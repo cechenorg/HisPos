34eb324808803279db9ffcab6d142d51659a40df
世彬 洪 <touycvbn95147@gmail.com>
2018-09-27 16:50:21 +0800

    yy

--- H2_STOCK_MANAGE/ProductPurchase/BatchNumberDialog.xaml.cs
+++ H2_STOCK_MANAGE/ProductPurchase/BatchNumberDialog.xaml.cs
@@ -59,14 +59,37 @@ namespace His_Pos.H2_STOCK_MANAGE.ProductPurchase
 
         private void ReturnAmount_PreviewKeyDown(object sender, KeyEventArgs e)
         {
-            if(!IsNumbers(e.Key))
+            if(!IsKeyAvailable(e.Key))
+            {
                 e.Handled = true;
+                return;
+            }
+
+            TextBox textBox = sender as TextBox;
+
+            if (textBox is null) return;
+
+            if (textBox.Text.Equals("0") && IsNumbers(e.Key))
+                textBox.Text = "";
+            else if(textBox.Text.Length == 1 && (e.Key == Key.Back || e.Key == Key.Delete))
+            {
+                textBox.Text = "0";
+                e.Handled = true;
+            }
         }
 
         private bool IsNumbers(Key key)
         {
             if (key >= Key.D0 && key <= Key.D9) return true;
             if (key >= Key.NumPad0 && key <= Key.NumPad9) return true;
+
+            return false;
+        }
+
+        private bool IsKeyAvailable(Key key)
+        {
+            if (IsNumbers(key)) return true;
+            if (key == Key.Back || key == Key.Delete || key == Key.Left || key == Key.Right || key == Key.OemPeriod || key == Key.Decimal) return true;
 
             return false;
         }
