e17f935806137a893089475cdac7e026ea20ffc2
世彬 洪 <touycvbn95147@gmail.com>
2018-10-16 15:52:23 +0800

    setting tab ui ok

--- SystemSettings/SettingWindow.xaml.cs
+++ SystemSettings/SettingWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,51 @@ namespace His_Pos.SystemSettings
     /// </summary>
     public partial class SettingWindow : Window
     {
+        private StackPanel CurrentStack { get; set; }
+
         public SettingWindow()
         {
             InitializeComponent();
+
+            CurrentStack = MyPharmacyStack;
+
+            UpdateContentControl();
+        }
+
+        private void UpdateContentControl()
+        {
+            SelectTab();
+
+        }
+        
+        private void SelectTab()
+        {
+            CurrentStack.Background = Brushes.LightGray;
+
+            Label label = CurrentStack.Children.OfType<Label>().ToList()[0];
+            label.Foreground = Brushes.DimGray;
+        }
+
+        private void ClearSelectTab()
+        {
+            CurrentStack.Background = Brushes.Transparent;
+
+            Label label = CurrentStack.Children.OfType<Label>().ToList()[0];
+            label.Foreground = Brushes.DarkGray;
+        }
+
+        private void SettingTab_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is null) return;
+
+            StackPanel stackPanel = sender as StackPanel;
+
+            if (CurrentStack != null)
+                ClearSelectTab();
+
+            CurrentStack = stackPanel;
+
+            UpdateContentControl();
         }
     }
 }
