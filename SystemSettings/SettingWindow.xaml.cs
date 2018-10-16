c81adf317f5218251c77c8466a554705670a7cbc
世彬 洪 <touycvbn95147@gmail.com>
2018-10-16 16:56:29 +0800

    switching setting ok

--- SystemSettings/SettingWindow.xaml.cs
+++ SystemSettings/SettingWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,29 +13,75 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using His_Pos.Class;
+using His_Pos.SystemSettings.SettingControl;
 
 namespace His_Pos.SystemSettings
 {
     /// <summary>
     /// SettingWindow.xaml 的互動邏輯
     /// </summary>
-    public partial class SettingWindow : Window
+    public partial class SettingWindow : Window, INotifyPropertyChanged
     {
+        #region ----- Define Variables -----
         private StackPanel CurrentStack { get; set; }
+        private UserControl currentControl;
+
+        public UserControl CurrentControl
+        {
+            get { return currentControl; }
+            set
+            {
+                currentControl = value;
+                NotifyPropertyChanged("CurrentControl");
+            }
+        }
+
+        private PrinterControl printerControl = new PrinterControl();
+        private DatabaseControl databaseControl = new DatabaseControl();
+        private MyPharmacyControl myPharmacyControl = new MyPharmacyControl();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+        #endregion
 
         public SettingWindow()
         {
             InitializeComponent();
+
+            DataContext = this;
 
             CurrentStack = MyPharmacyStack;
 
             UpdateContentControl();
         }
 
+        #region ----- Update View -----
         private void UpdateContentControl()
         {
             SelectTab();
 
+            SettingTabs settingTab = (SettingTabs) Int16.Parse(CurrentStack.Tag.ToString());
+
+            switch (settingTab)
+            {
+                case SettingTabs.MyPharmacy:
+                    CurrentControl = myPharmacyControl;
+                    break;
+                case SettingTabs.Printer:
+                    CurrentControl = printerControl;
+                    break;
+                case SettingTabs.Database:
+                    CurrentControl = databaseControl;
+                    break;
+            }
         }
         
         private void SelectTab()
@@ -52,6 +99,7 @@ namespace His_Pos.SystemSettings
             Label label = CurrentStack.Children.OfType<Label>().ToList()[0];
             label.Foreground = Brushes.DarkGray;
         }
+        #endregion
 
         private void SettingTab_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
