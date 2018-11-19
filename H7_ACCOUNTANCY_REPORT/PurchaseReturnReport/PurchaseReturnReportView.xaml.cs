12b3442e12b9c319507294316859492af72c9823
世彬 洪 <touycvbn95147@gmail.com>
2018-11-19 16:53:02 +0800

    aa

--- H7_ACCOUNTANCY_REPORT/PurchaseReturnReport/PurchaseReturnReportView.xaml.cs
+++ H7_ACCOUNTANCY_REPORT/PurchaseReturnReport/PurchaseReturnReportView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,29 @@ namespace His_Pos.H7_ACCOUNTANCY_REPORT.PurchaseReturnReport
     /// <summary>
     /// PurchaseReturnReportView.xaml 的互動邏輯
     /// </summary>
-    public partial class PurchaseReturnReportView : UserControl
+    public partial class PurchaseReturnReportView : UserControl, INotifyPropertyChanged
     {
+        #region ----- Define Variables -----
+
+        private bool IsFirstStack { get; set; } = true;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+        #endregion
+
         public PurchaseReturnReportView()
         {
             InitializeComponent();
+
+
         }
+
+
     }
 }
