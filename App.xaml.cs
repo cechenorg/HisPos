246cde86c01d253e542f28b832a4b1e56cc340f4
xilian <l1o2u3i45s1@gmail.com>
2018-12-04 18:23:50 +0800

    OK

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Security.Principal;
+using System.Windows;
 
 namespace His_Pos
 {
@@ -7,5 +11,44 @@ namespace His_Pos
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            CheckAdministrator();
+            //如果不是管理员，程序会直接退出，并使用管理员身份重新运行。
+            StartupUri = new Uri("LoginPage.xaml", UriKind.RelativeOrAbsolute);
+        }
+        private void CheckAdministrator()
+        {
+            var wi = WindowsIdentity.GetCurrent();
+            var wp = new WindowsPrincipal(wi);
+
+            bool runAsAdmin = wp.IsInRole(WindowsBuiltInRole.Administrator);
+
+            if (!runAsAdmin)
+            {
+                // It is not possible to launch a ClickOnce app as administrator directly,
+                // so instead we launch the app as administrator in a new process.
+                var processInfo = new ProcessStartInfo(Assembly.GetExecutingAssembly().CodeBase);
+
+                // The following properties run the new process as administrator
+                processInfo.UseShellExecute = true;
+                processInfo.Verb = "runas";
+
+                // Start the new process
+                try
+                {
+                    Process.Start(processInfo);
+                }
+                catch (Exception ex)
+                {
+                    MessageWindow messageWindow = new MessageWindow(ex.Message,Class.MessageType.ERROR);
+                }
+
+                // Shut down the current process
+                Environment.Exit(0);
+            }
+        }
     }
 }
