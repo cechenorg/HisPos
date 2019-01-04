a5e60b37adb2d0d6f2fd424c369acc4bc66322fd
Frankemg <fk035372150@gmail.com>
2019-01-04 21:37:24 +0800

    OK

---
+++ NewClass/IcCard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace His_Pos.NewClass
+{
+    public class IcCard
+    {
+        public IcCard() { }
+        public string CardNumber { get;}//卡片號碼
+        public string IcNumber { get; set; }//身分證字號
+        public DateTime CardReleaseDate { get;}//發卡日期
+        public DateTime ValidityPeriod { get;}//卡片有效期限
+        public int AvailableTimes { get;}//就醫可用次數
+
+    }
+}
--- NewClass/Prescription.cs
+++ NewClass/Prescription.cs
@@ -1,20 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Data;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 
 namespace His_Pos.NewClass
 {
-    public class Prescription
+    public class Prescription:INotifyPropertyChanged
     {
         public Prescription() { }
 
         public Prescription(DataRow r)
         {
-
+            Id = (int)r[""];
         }
+        public int Id { get; }
+        public string SourceId { get;}//合作診所.慢箋Id
+        private Customer patient;//病患
+        public Customer Patient
+        {
+            get => patient;
+            set
+            {
+                patient = value;
+                OnPropertyChanged(nameof(Patient));
+            }
+        }
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
     }
 }
