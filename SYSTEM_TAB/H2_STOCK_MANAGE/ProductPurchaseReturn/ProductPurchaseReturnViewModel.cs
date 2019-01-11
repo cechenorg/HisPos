63b6106a022faddfc56050d84e9d352cf49d2d53
世彬 洪 <touycvbn95147@gmail.com>
2019-01-11 09:22:18 +0800

    hi

---
+++ SYSTEM_TAB/H2_STOCK_MANAGE/ProductPurchaseReturn/ProductPurchaseReturnViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace His_Pos.SYSTEM_TAB.H2_STOCK_MANAGE.ProductPurchaseReturn
+{
+    public class ProductPurchaseReturnViewModel : ViewModelBase
+    {
+        public RelayCommand AddOrderCommand { get; set; }
+        public RelayCommand DeleteOrderCommand { get; set; }
+        public RelayCommand SaveOrderCommand { get; set; }
+        public RelayCommand ToNextStatusCommand { get; set; }
+        public RelayCommand AllProcessingOrderToDoneCommand { get; set; }
+
+        public ProductPurchaseReturnViewModel()
+        {
+
+        }
+    }
+}
