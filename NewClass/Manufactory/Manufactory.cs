32812326917bdade124fb71e31af842dc623d909
世彬 洪 <touycvbn95147@gmail.com>
2019-01-11 14:17:29 +0800

    gg

---
+++ NewClass/Manufactory/Manufactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace His_Pos.NewClass.Manufactory
+{
+    public class Manufactory
+    {
+        public Manufactory(DataRow dataRow)
+        {
+
+        }
+
+        #region ----- Define Variables -----
+        public string ID { get; set; }
+        public string Name { get; set; }
+        #endregion
+    }
+}
