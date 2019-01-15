d44f5f028dbab92f9e52340503e79c331bde4fd9
世彬 洪 <touycvbn95147@gmail.com>
2019-01-15 20:18:00 +0800

    yo

---
+++ NewClass/Product/ProductStruct.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace His_Pos.NewClass.Product
+{
+    public struct ProductStruct
+    {
+        public ProductStruct(DataRow row)
+        {
+            ID = row.Field<string>("");
+            ChineseName = row.Field<string>("");
+            EnglishName = row.Field<string>("");
+            Inventory = row.Field<int>("");
+            SafeAmount = row.Field<int>("");
+            BasicAmount = row.Field<int>("");
+            NHIPrice = row.Field<double>("");
+            SellPrice = row.Field<double>("");
+        }
+
+        public string ID { get; set; }
+        public string ChineseName { get; set; }
+        public string EnglishName { get; set; }
+        public string FullName
+        {
+            get { return EnglishName.Substring(0, EnglishName.IndexOf(" ")) + ChineseName; }
+        }
+        public int Inventory { get; set; }
+        public int SafeAmount { get; set; }
+        public int BasicAmount { get; set; }
+        public double NHIPrice { get; set; }
+        public double SellPrice { get; set; }
+    }
+}
