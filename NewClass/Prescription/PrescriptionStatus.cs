befa2ba61f9b0865c8f6465fa1482ff63ba53f41
xilian <l1o2u3i45s1@gmail.com>
2019-01-23 19:36:09 +0800

    OK

--- NewClass/Prescription/PrescriptionStatus.cs
+++ NewClass/Prescription/PrescriptionStatus.cs
@@ -15,8 +15,9 @@ namespace His_Pos.NewClass.Prescription {
         public bool IsGetCard { get; set; } //是否讀卡
         public bool IsDeclare { get; set; } //是否申報
         public bool IsRead { get; set; }//是否已讀
-        public bool IsSendToSingde { get; set; }//是否傳送藥健康
+        public bool IsSendToSingde { get; set; }//是否傳送過藥健康
         public bool IsAdjust { get; set; }//是否調劑.扣庫
         public bool IsRegister { get; set; } //是否登錄
+        public bool IsSendOrder { get; set; } //判斷是否傳送要健康
     }
 }
