2b578bbe169d78bc31d1f9df14c286b4d9b03aea
世彬 洪 <touycvbn95147@gmail.com>
2019-01-30 17:31:27 +0800

    yo

--- ChromeTabViewModel/ViewModelMainWindow.cs
+++ ChromeTabViewModel/ViewModelMainWindow.cs
@@ -27,6 +27,7 @@ using His_Pos.NewClass.Prescription.Treatment.PrescriptionCase;
 using His_Pos.NewClass.Prescription.Treatment.SpecialTreat;
 using His_Pos.NewClass.Product.Medicine.Position;
 using His_Pos.NewClass.Product.Medicine.Usage;
+using His_Pos.Service;
 using Microsoft.Reporting.WinForms;
 using Prescription = His_Pos.NewClass.Prescription.Prescription;
 using StringRes = His_Pos.Properties.Resources;
@@ -189,8 +190,12 @@ namespace His_Pos.ChromeTabViewModel
             {
                 IsBusy = false;
             };
+            NewFunction.ExceptionLog("IsBusyStart");
+
             IsBusy = true;
             worker.RunWorkerAsync();
+
+            NewFunction.ExceptionLog("IsBusyEnd");
         }
 
         public static Institution GetInstitution(string id)
