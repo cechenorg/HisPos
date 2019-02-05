bf188a11bfd5eeddc5e6a3ec73da739ca61afb78
Frankemg <fk035372150@gmail.com>
2019-02-05 15:55:44 +0800

    ok

--- SYSTEM_TAB/H1_DECLARE/DeclareFileManage/DeclareFileManageViewModel.cs
+++ SYSTEM_TAB/H1_DECLARE/DeclareFileManage/DeclareFileManageViewModel.cs
@@ -4,14 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using His_Pos.ChromeTabViewModel;
+using His_Pos.NewClass.Person.MedicalPerson;
+using His_Pos.NewClass.Prescription.Treatment.AdjustCase;
 
 namespace His_Pos.SYSTEM_TAB.H1_DECLARE.DeclareFileManage
 {
     public class DeclareFileManageViewModel:TabBase
     {
+        #region Variables
         public override TabBase getTab()
         {
             return this;
+        }
+        public MedicalPersonnels MedicalPersonnels { get; set; }
+        public AdjustCases AdjustCases { get; set; }
+        #endregion
+        public DeclareFileManageViewModel()
+        {
+
         }
     }
 }
