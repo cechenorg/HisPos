5ab658cc570be692369590a60d20131f66c618c1
xilian <user@MSI>
2019-02-11 17:57:05 +0800

    OK

--- SYSTEM_TAB/H7_ACCOUNTANCY_REPORT/ControlMedicineDeclare/ControlMedicineDeclareViewModel.cs
+++ SYSTEM_TAB/H7_ACCOUNTANCY_REPORT/ControlMedicineDeclare/ControlMedicineDeclareViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using GalaSoft.MvvmLight;
+using His_Pos.ChromeTabViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +7,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace His_Pos.SYSTEM_TAB.H7_ACCOUNTANCY_REPORT.ControlMedicineDeclare {
-    public class ControlMedicineDeclareViewModel:ViewModelBase {
+    public class ControlMedicineDeclareViewModel:TabBase {
+        public override TabBase getTab() {
+            return this;
+        }
         public ControlMedicineDeclareViewModel() {
 
         }
