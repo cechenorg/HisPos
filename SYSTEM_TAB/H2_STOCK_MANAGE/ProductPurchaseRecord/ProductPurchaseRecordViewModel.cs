aa8f3034947a5745c46f11352607c136c61de13a
世彬 洪 <touycvbn95147@gmail.com>
2019-02-14 19:06:00 +0800

    record master ui okk

--- SYSTEM_TAB/H2_STOCK_MANAGE/ProductPurchaseRecord/ProductPurchaseRecordViewModel.cs
+++ SYSTEM_TAB/H2_STOCK_MANAGE/ProductPurchaseRecord/ProductPurchaseRecordViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using GalaSoft.MvvmLight.Command;
 using His_Pos.ChromeTabViewModel;
+using His_Pos.NewClass.StoreOrder;
 
 namespace His_Pos.SYSTEM_TAB.H2_STOCK_MANAGE.ProductPurchaseRecord
 {
@@ -17,10 +18,27 @@ namespace His_Pos.SYSTEM_TAB.H2_STOCK_MANAGE.ProductPurchaseRecord
         #endregion
 
         #region ----- Define Variables -----
+
+        #region ///// Search Variables /////
         public DateTime? SearchStartDate { get; set; }
         public DateTime? SearchEndDate { get; set; }
         public string SearchOrderID { get; set; }
         public string SearchID { get; set; }
+        #endregion
+        
+        private StoreOrder currentStoreOrder;
+        private StoreOrders storeOrderCollection;
+        
+        public StoreOrders StoreOrderCollection
+        {
+            get { return storeOrderCollection; }
+            set { Set(() => StoreOrderCollection, ref storeOrderCollection, value); }
+        }
+        public StoreOrder CurrentStoreOrder
+        {
+            get { return currentStoreOrder; }
+            set { Set(() => CurrentStoreOrder, ref currentStoreOrder, value); }
+        }
         #endregion
 
         #region ----- Define Actions -----
