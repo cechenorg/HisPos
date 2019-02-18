58f422c525358d691a488737f98b3b95deb9b236
xilian <user@MSI>
2019-02-18 16:36:41 +0800

    OK

--- NewClass/CashFlow/CashFlow.cs
+++ NewClass/CashFlow/CashFlow.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -6,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace His_Pos.NewClass.CashFlow {
-    public class CashFlow {
+    public class CashFlow : ObservableObject {
         public CashFlow() { }
         public CashFlow(DataRow r) {
             Date = r.Field<DateTime>("CashFlow_Time");
@@ -19,13 +20,68 @@ namespace His_Pos.NewClass.CashFlow {
             DepositValue = Math.Round(r.Field<decimal>("DepositValue"), 2);
         }
         public DateTime Date { get; set; }
-        public decimal CopaymentValue { get; set; }
-        public decimal ClinicCopaymentValue { get; set; }
-        public decimal PaySelfValue { get; set; }
-        public decimal ClinicPaySelfValue { get; set; }
-        public decimal MedServiceValue { get; set; }
-        public decimal ClinicMedServiceValue { get; set; }
-        public decimal DepositValue { get; set; }
+        private decimal copaymentValue;
+        public decimal CopaymentValue {
+            get => copaymentValue;
+            set
+            {
+                Set(() => CopaymentValue, ref copaymentValue, value);
+            }
+        }
+        private decimal clinicCopaymentValue;
+        public decimal ClinicCopaymentValue
+        {
+            get => clinicCopaymentValue;
+            set
+            {
+                Set(() => ClinicCopaymentValue, ref clinicCopaymentValue, value);
+            }
+        }
+        private decimal paySelfValue;
+        public decimal PaySelfValue
+        {
+            get => paySelfValue;
+            set
+            {
+                Set(() => PaySelfValue, ref paySelfValue, value);
+            }
+        }
+        private decimal clinicPaySelfValue;
+        public decimal ClinicPaySelfValue
+        {
+            get => clinicPaySelfValue;
+            set
+            {
+                Set(() => ClinicPaySelfValue, ref clinicPaySelfValue, value);
+            }
+        }
+        private decimal medServiceValue;
+        public decimal MedServiceValue
+        {
+            get => medServiceValue;
+            set
+            {
+                Set(() => MedServiceValue, ref medServiceValue, value);
+            }
+        }
+        private decimal clinicMedServiceValue;
+        public decimal ClinicMedServiceValue
+        {
+            get => clinicMedServiceValue;
+            set
+            {
+                Set(() => ClinicMedServiceValue, ref clinicMedServiceValue, value);
+            }
+        }
+        private decimal depositValue;
+        public decimal DepositValue
+        {
+            get => depositValue;
+            set
+            {
+                Set(() => DepositValue, ref depositValue, value);
+            }
+        }
 
        
     }
