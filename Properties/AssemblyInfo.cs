c176a77b7ea2cbbd703e597a5bee62d4f10a63af
admin <admin@DESKTOP-GVIDQBE>
2019-02-21 19:00:56 +0800

    K

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // 您可以指定所有的值，或將組建編號或修訂編號設為預設值
 // 指定為預設值: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.2.8")]
-[assembly: AssemblyFileVersion("2.0.2.8")]
+[assembly: AssemblyVersion("2.0.2.11")]
+[assembly: AssemblyFileVersion("2.0.2.11")]
