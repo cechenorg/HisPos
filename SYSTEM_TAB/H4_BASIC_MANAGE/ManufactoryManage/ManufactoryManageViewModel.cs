e1eb5056199b648fced41cb4656b8a0213d33051
世彬 洪 <touycvbn95147@gmail.com>
2019-02-28 03:37:54 +0800

    yoyo

--- SYSTEM_TAB/H4_BASIC_MANAGE/ManufactoryManage/ManufactoryManageViewModel.cs
+++ SYSTEM_TAB/H4_BASIC_MANAGE/ManufactoryManage/ManufactoryManageViewModel.cs
@@ -1,12 +1,97 @@
-﻿using His_Pos.ChromeTabViewModel;
+﻿using GalaSoft.MvvmLight.CommandWpf;
+using His_Pos.ChromeTabViewModel;
 
 namespace His_Pos.SYSTEM_TAB.H4_BASIC_MANAGE.ManufactoryManage
 {
-    class ManufactoryManageViewModel : TabBase
+    public class ManufactoryManageViewModel : TabBase
     {
         public override TabBase getTab()
         {
             return this;
         }
+
+        #region ----- Define Commands -----
+        public RelayCommand SearchCommand { get; set; }
+        public RelayCommand AddManufactoryCommand { get; set; }
+        public RelayCommand DeleteManufactoryCommand { get; set; }
+        public RelayCommand AddManufactoryPrincipalCommand { get; set; }
+        public RelayCommand DeleteManufactoryPrincipalCommand { get; set; }
+        public RelayCommand DataChangedCommand { get; set; }
+        public RelayCommand ConfirmChangeCommand { get; set; }
+        public RelayCommand CancelChangeCommand { get; set; }
+        #endregion
+
+        #region ----- Define Variables -----
+        private bool isDataChanged;
+
+        public bool IsDataChanged
+        {
+            get { return isDataChanged; }
+            set
+            {
+                Set(() => IsDataChanged, ref isDataChanged, value);
+                CancelChangeCommand.RaiseCanExecuteChanged();
+                ConfirmChangeCommand.RaiseCanExecuteChanged();
+            }
+        }
+        #endregion
+
+        public ManufactoryManageViewModel()
+        {
+            RegisterCommand();
+        }
+
+        #region ----- Define Actions -----
+        private void SearchAction()
+        {
+
+        }
+        private void AddManufactoryAction()
+        {
+
+        }
+        private void DeleteManufactoryAction()
+        {
+
+        }
+        private void AddManufactoryPrincipalAction()
+        {
+
+        }
+        private void DeleteManufactoryPrincipalAction()
+        {
+
+        }
+        private void DataChangedAction()
+        {
+            IsDataChanged = true;
+        }
+        private void ConfirmChangeAction()
+        {
+            IsDataChanged = false;
+        }
+        private void CancelChangeAction()
+        {
+            IsDataChanged = false;
+        }
+        #endregion
+
+        #region ----- Define Functions -----
+        private void RegisterCommand()
+        {
+            SearchCommand = new RelayCommand(SearchAction);
+            AddManufactoryCommand = new RelayCommand(SearchAction);
+            DeleteManufactoryCommand = new RelayCommand(SearchAction);
+            AddManufactoryPrincipalCommand = new RelayCommand(SearchAction);
+            DeleteManufactoryPrincipalCommand = new RelayCommand(SearchAction);
+            DataChangedCommand = new RelayCommand(SearchAction);
+            ConfirmChangeCommand = new RelayCommand(SearchAction, IsManufactoryDataChanged);
+            CancelChangeCommand = new RelayCommand(SearchAction, IsManufactoryDataChanged);
+        }
+        private bool IsManufactoryDataChanged()
+        {
+            return IsDataChanged;
+        }
+        #endregion
     }
 }
