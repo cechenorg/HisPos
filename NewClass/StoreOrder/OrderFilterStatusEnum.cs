eb139c8b54ba7582669c19f6cf96c85d51784bb8
世彬 洪 <touycvbn95147@gmail.com>
2019-03-04 12:02:52 +0800

    filter storeorder

---
+++ NewClass/StoreOrder/OrderFilterStatusEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace His_Pos.NewClass.StoreOrder
+{
+    public enum OrderFilterStatusEnum
+    {
+        ERROR = 0,
+        UNPROCESSING = 1,
+        WAITING = 2,
+        PROCESSING = 3,
+        ALL = 4
+    }
+}
