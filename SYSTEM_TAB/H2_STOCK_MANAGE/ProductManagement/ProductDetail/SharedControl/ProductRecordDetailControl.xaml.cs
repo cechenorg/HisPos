c874a4445d684aefd75f4050e682f19246d5ed86
世彬 洪 <touycvbn95147@gmail.com>
2019-04-28 21:49:38 +0800

    can double click

--- SYSTEM_TAB/H2_STOCK_MANAGE/ProductManagement/ProductDetail/SharedControl/ProductRecordDetailControl.xaml.cs
+++ SYSTEM_TAB/H2_STOCK_MANAGE/ProductManagement/ProductDetail/SharedControl/ProductRecordDetailControl.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GalaSoft.MvvmLight.Messaging;
+using His_Pos.ChromeTabViewModel;
+using His_Pos.NewClass.Product.ProductManagement;
+using His_Pos.SYSTEM_TAB.H1_DECLARE.PrescriptionSearch.PrescriptionEditWindow;
+using His_Pos.SYSTEM_TAB.H2_STOCK_MANAGE.ProductPurchaseRecord;
 
 namespace His_Pos.SYSTEM_TAB.H2_STOCK_MANAGE.ProductManagement.ProductDetail.SharedControl
 {
@@ -23,6 +28,26 @@ namespace His_Pos.SYSTEM_TAB.H2_STOCK_MANAGE.ProductManagement.ProductDetail.Sha
         public ProductRecordDetailControl()
         {
             InitializeComponent();
+        }
+
+        private void ShowRecordDetail(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is null) return;
+
+            switch (((ProductInventoryRecord)(sender as DataGridRow).Item).Type)
+            {
+                case ProductInventoryRecordType.PurchaseReturn:
+                    ProductPurchaseRecordViewModel viewModel = (App.Current.Resources["Locator"] as ViewModelLocator).ProductPurchaseRecord;
+
+                    Messenger.Default.Send(new NotificationMessage<string>(this, viewModel, ((ProductInventoryRecord)(sender as DataGridRow).Item).Name, ""));
+                    break;
+                case ProductInventoryRecordType.Prescription:
+                    PrescriptionEditWindow prescriptionEditWindow = new PrescriptionEditWindow(int.Parse(((ProductInventoryRecord)(sender as DataGridRow).Item).ID));
+                    prescriptionEditWindow.ShowDialog();
+                    break;
+                case ProductInventoryRecordType.StockTaking:
+                    break;
+            }
         }
     }
 }
