a35315ea4cb5a181f89703ef20d95fd9b96d6531
xilian <user@MSI>
2019-05-07 17:39:24 +0800

    拆庫~

---
+++ SYSTEM_TAB/H2_STOCK_MANAGE/ProductManagement/ProductDetail/SharedWindow/ProductGroupSettingWindow/ProductGroupSettingWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace His_Pos.SYSTEM_TAB.H2_STOCK_MANAGE.ProductManagement.ProductDetail.SharedWindow.ProductGroupSettingWindow {
+    public class ProductGroupSettingWindowViewModel : ObservableObject {
+        #region Var
+        private bool isSplit;
+        public bool IsSpilt {
+            get { return isSplit; }
+            set { Set(() => IsSpilt, ref isSplit, value); }
+        }
+        #endregion
+
+
+        public ProductGroupSettingWindowViewModel() { }
+    }
+}
