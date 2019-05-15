db9588cc0cfe99cde5ee62e36380fb1836463e8d
世彬 洪 <touycvbn95147@gmail.com>
2019-05-15 20:42:59 +0800

    StockTakingPlan control create

---
+++ SYSTEM_TAB/H3_STOCKTAKING/StockTakingPlan/StockTakingPlanViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using His_Pos.ChromeTabViewModel;
+
+namespace His_Pos.SYSTEM_TAB.H3_STOCKTAKING.StockTakingPlan
+{
+    public class StockTakingPlanViewModel : TabBase
+    {
+        public override TabBase getTab()
+        {
+            return this;
+        }
+    }
+}
