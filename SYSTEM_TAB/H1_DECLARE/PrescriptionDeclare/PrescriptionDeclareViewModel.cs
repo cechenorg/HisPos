842ed1c8a3f82ad706c1f1da8850319046cc77cc
汎遠 宋 <fk035372150@gmail.com>
2019-05-18 21:44:28 +0800

    處方登錄修改顧客詳細資料 編輯結束重新查詢 列印藥袋更新電話列印順序為 手機 => 家電 => 聯絡備註

--- SYSTEM_TAB/H1_DECLARE/PrescriptionDeclare/PrescriptionDeclareViewModel.cs
+++ SYSTEM_TAB/H1_DECLARE/PrescriptionDeclare/PrescriptionDeclareViewModel.cs
@@ -479,6 +479,9 @@ namespace His_Pos.SYSTEM_TAB.H1_DECLARE.PrescriptionDeclare
         private void ShowCustomerDetailAction() {
             if (CurrentPrescription.Patient.ID == -1) return;
             CustomerDetailWindow customerDetailWindow = new CustomerDetailWindow(CurrentPrescription.Patient.ID);
+            MainWindow.ServerConnection.OpenConnection();
+            CurrentPrescription.Patient.GetCustomerByCusId(CurrentPrescription.Patient.ID);
+            MainWindow.ServerConnection.CloseConnection();
         }
         private void SearchCusAction(object sender)
         {
