9c62e744c23b8a42911e244fb7d76ac14928c76b
世彬 洪 <touycvbn95147@gmail.com>
2019-05-22 23:40:54 +0800

    type ui

---
+++ SYSTEM_TAB/H2_STOCK_MANAGE/ProductTypeManage/ProductTypeManageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using His_Pos.ChromeTabViewModel;
+
+namespace His_Pos.SYSTEM_TAB.H2_STOCK_MANAGE.ProductTypeManage
+{
+    class ProductTypeManageViewModel : TabBase
+    {
+        public override TabBase getTab()
+        {
+            return this;
+        }
+
+        #region ----- Define Commands -----
+        #endregion
+
+        #region ----- Define Variables -----
+        #endregion
+
+        #region ----- Define Actions -----
+        #endregion
+
+        #region ----- Define Functions -----
+        #endregion
+    }
+}
