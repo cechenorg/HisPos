e910c6a4b404aeece035205ee5722bf0ecd4e39f
xilian <l1o2u3i45s1@gmail.com>
2019-05-23 21:04:38 +0800

    顧客處方修改紀錄 OK

---
+++ SYSTEM_TAB/INDEX/CustomerPrescriptionChangedWindow/CustomerPrescriptionChangedWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace His_Pos.SYSTEM_TAB.INDEX.CustomerPrescriptionChangedWindow
+{
+    /// <summary>
+    /// CustomerPrescriptionChangedWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class CustomerPrescriptionChangedWindow : Window
+    {
+        public CustomerPrescriptionChangedWindow(int cusID)
+        {
+            InitializeComponent();
+            CustomerPrescriptionChangedViewModel customerPrescriptionChangedViewModel = new CustomerPrescriptionChangedViewModel(cusID);
+            DataContext = customerPrescriptionChangedViewModel;
+            ShowDialog();
+        }
+    }
+}
