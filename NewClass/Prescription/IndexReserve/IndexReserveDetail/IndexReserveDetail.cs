1ddf09105a7499838382b8f23ae50a5f2c59ff12
xilian <l1o2u3i45s1@gmail.com>
2019-05-24 00:49:05 +0800

    OntheWay

--- NewClass/Prescription/IndexReserve/IndexReserveDetail/IndexReserveDetail.cs
+++ NewClass/Prescription/IndexReserve/IndexReserveDetail/IndexReserveDetail.cs
@@ -11,12 +11,15 @@ namespace His_Pos.NewClass.Prescription.IndexReserve.IndexReserveDetail
    public class IndexReserveDetail : Product.Product {
         public IndexReserveDetail(DataRow r):base(r) { 
             Stock = r.Field<string>("Inventory");
+            OntheWay = r.Field<string>("OntheWay");
             Amount = r.Field<double>("TotalAmount"); 
         }
 
         public string Id { get; set; }
         public string Name { get; set; }
         public string Stock { get; set; }
+        public string OntheWay { get; set; }
+        
         public double Amount { get; set; }
     }
 }
