55f141e3c8659c1a57ec372080108b5310865fac
Frank <fk035372150@gmail.com>
2019-05-28 19:00:53 +0800

    OK

---
+++ SYSTEM_TAB/H1_DECLARE/PrescriptionDeclare/FunctionWindowRefactoring/CustomerPrescriptionWindow/CustomerPrescriptionViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace His_Pos.SYSTEM_TAB.H1_DECLARE.PrescriptionDeclare.FunctionWindowRefactoring.CustomerPrescriptionWindow
+{
+    public class CustomerPrescriptionViewModel : ViewModelBase
+    {
+    }
+}
---
+++ SYSTEM_TAB/H1_DECLARE/PrescriptionDeclare/FunctionWindowRefactoring/CustomerPrescriptionWindow/CustomerPrescriptionWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace His_Pos.SYSTEM_TAB.H1_DECLARE.PrescriptionDeclare.FunctionWindowRefactoring.CustomerPrescriptionWindow
+{
+    /// <summary>
+    /// CustomerPrescriptionWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class CustomerPrescriptionWindow : Window
+    {
+        public CustomerPrescriptionWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
