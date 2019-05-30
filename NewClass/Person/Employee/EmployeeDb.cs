1754826988f53a1ba558912426ec05a70ef98b6f
xilian <l1o2u3i45s1@gmail.com>
2019-05-30 18:26:42 +0800

    employee done

--- NewClass/Person/Employee/EmployeeDb.cs
+++ NewClass/Person/Employee/EmployeeDb.cs
@@ -50,6 +50,12 @@ namespace His_Pos.NewClass.Person.Employee
                 SyncData();
             } 
         }
+        public static DataTable GetEmployeeNewAccount()
+        {
+            List<SqlParameter> parameterList = new List<SqlParameter>();
+           return MainWindow.ServerConnection.ExecuteProc("[Get].[EmployeeNewAccount]", parameterList); 
+        }
+        
         public static DataTable EmployeeLogin(string account,string password) {
             List<SqlParameter> parameterList = new List<SqlParameter>();
             parameterList.Add(new SqlParameter("Account", account));
@@ -57,6 +63,20 @@ namespace His_Pos.NewClass.Person.Employee
             var table = MainWindow.ServerConnection.ExecuteProc("[Get].[EmployeeLogin]",parameterList);  
             return table;
         }
+        public static DataTable CheckIdNumber(string idNumber)
+        {
+            List<SqlParameter> parameterList = new List<SqlParameter>();
+            parameterList.Add(new SqlParameter("IdNumber", idNumber)); 
+            var table = MainWindow.ServerConnection.ExecuteProc("[Get].[EmployeeCheckIdNumber]", parameterList);
+            return table;
+        }
+        public static DataTable CheckEmployeeAccountSame(string account)
+        {
+            List<SqlParameter> parameterList = new List<SqlParameter>();
+            parameterList.Add(new SqlParameter("Account", account));
+            var table = MainWindow.ServerConnection.ExecuteProc("[Get].[CheckEmployeeAccountSame]", parameterList);
+            return table;
+        } 
         public static DataTable GetTabAuth(int AuthValue) {
             List<SqlParameter> parameterList = new List<SqlParameter>();
             parameterList.Add(new SqlParameter("AuthValue", AuthValue)); 
