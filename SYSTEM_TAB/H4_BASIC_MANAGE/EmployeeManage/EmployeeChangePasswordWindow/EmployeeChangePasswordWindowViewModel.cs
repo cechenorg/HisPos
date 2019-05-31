c3796ae706b14d4de16f33cf168c72890ca65af5
xilian <user@MSI>
2019-05-31 16:50:51 +0800

    EMP換密碼中

---
+++ SYSTEM_TAB/H4_BASIC_MANAGE/EmployeeManage/EmployeeChangePasswordWindow/EmployeeChangePasswordWindowViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace His_Pos.SYSTEM_TAB.H4_BASIC_MANAGE.EmployeeManage.EmployeeChangePasswordWindow {
+    class EmployeeChangePasswordWindowViewModel {
+    }
+}
