246a39bc902aff7d69c855ab49feab3fe9cdd3d0
admin <admin@DESKTOP-GVIDQBE>
2019-06-14 17:35:15 +0800

    export setting ok

--- Service/ExportService/ExportDataTableExcelSetting.cs
+++ Service/ExportService/ExportDataTableExcelSetting.cs
@@ -12,20 +12,21 @@ namespace His_Pos.Service.ExportService
     class ExportDataTableExcelSetting : ExportExcelSetting
     {
         #region ----- Define Variables -----
-        public new DataTable Data { get; set; }
+        private new DataTable Data;
 
         #region ///// Header Variables /////
-        public int HeaderFontSize { get; set; } = 12;
-        public Color HeaderFontColor { get; set; } = Color.Black;
-        public Color HeaderBackGroundColor { get; set; } = Color.LightBlue;
-        public bool HeaderIsBold { get; set; } = true;
+        private string[] headers;
+        private int headerFontSize = 12;
+        private Color headerFontColor = Color.Black;
+        private Color headerBackGroundColor = Color.LightBlue;
+        private bool headerIsBold = true;
         #endregion
 
         #region ///// Data Variables /////
-        public int DataFontSize { get; set; } = 12;
-        public Color DataFontColor { get; set; } = Color.Black;
-        public Color DataBackGroundColor { get; set; } = Color.Transparent;
-        public bool DataIsBold { get; set; } = false;
+        private int dataFontSize = 12;
+        private Color dataFontColor = Color.Black;
+        private Color dataBackGroundColor = Color.Transparent;
+        private bool dataIsBold = false;
         #endregion
 
         #endregion
@@ -37,40 +38,98 @@ namespace His_Pos.Service.ExportService
             Column = column;
         }
 
+        #region ----- Define Fluent Interface Functions -----
+        public ExportDataTableExcelSetting SetHeaderFontSize(int _fontSize)
+        {
+            headerFontSize = _fontSize;
+            return this;
+        }
+        public ExportDataTableExcelSetting SetHeaderFontColor(Color _fontColor)
+        {
+            headerFontColor = _fontColor;
+            return this;
+        }
+        public ExportDataTableExcelSetting SetHeaderBackGroundColor(Color _backGroundColor)
+        {
+            headerBackGroundColor = _backGroundColor;
+            return this;
+        }
+        public ExportDataTableExcelSetting SetHeaderIsBold(bool _isBold)
+        {
+            headerIsBold = _isBold;
+            return this;
+        }
+        public ExportDataTableExcelSetting SetDataFontSize(int _fontSize)
+        {
+            dataFontSize = _fontSize;
+            return this;
+        }
+        public ExportDataTableExcelSetting SetDataFontColor(Color _fontColor)
+        {
+            dataFontColor = _fontColor;
+            return this;
+        }
+        public ExportDataTableExcelSetting SetDataBackGroundColor(Color _backGroundColor)
+        {
+            dataBackGroundColor = _backGroundColor;
+            return this;
+        }
+        public ExportDataTableExcelSetting SetDataIsBold(bool _isBold)
+        {
+            dataIsBold = _isBold;
+            return this;
+        }
+        public ExportDataTableExcelSetting SetHeaders(string[] _headers)
+        {
+            headers = _headers;
+            return this;
+        }
+        #endregion
+
         #region ----- Define Functions -----
         public override void InsertDataToSheet(ExcelWorksheet worksheet)
         {
-            for (int col = 0; col < Data.Columns.Count; col++)
-            {
-                worksheet.Cells[Row, Column + col].Style.Font.Bold = HeaderIsBold;
-                worksheet.Cells[Row, Column + col].Style.Font.Size = HeaderFontSize;
-                worksheet.Cells[Row, Column + col].Style.Font.Color.SetColor(HeaderFontColor);
-
-                if (HeaderBackGroundColor != Color.Transparent)
-                {
-                    worksheet.Cells[Row, Column + col].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
-                    worksheet.Cells[Row, Column + col].Style.Fill.BackgroundColor.SetColor(HeaderBackGroundColor);
-                }
-
-                worksheet.Cells[Row, Column + col].Value = Data.Columns[col].ColumnName;
-            }
-
-            for(int row = 0; row < Data.Rows.Count; row++)
+            SetHeader(worksheet);
+            SetData(worksheet);
+        }
+        private void SetData(ExcelWorksheet worksheet)
+        {
+            for (int row = 0; row < Data.Rows.Count; row++)
             {
                 for (int col = 0; col < Data.Columns.Count; col++)
                 {
-                    worksheet.Cells[Row + row + 1, Column + col].Style.Font.Bold = DataIsBold;
-                    worksheet.Cells[Row + row + 1, Column + col].Style.Font.Size = DataFontSize;
-                    worksheet.Cells[Row + row + 1, Column + col].Style.Font.Color.SetColor(DataFontColor);
+                    worksheet.Cells[Row + row + 1, Column + col].Style.Font.Bold = dataIsBold;
+                    worksheet.Cells[Row + row + 1, Column + col].Style.Font.Size = dataFontSize;
+                    worksheet.Cells[Row + row + 1, Column + col].Style.Font.Color.SetColor(dataFontColor);
 
-                    if (DataBackGroundColor != Color.Transparent)
+                    if (dataBackGroundColor != Color.Transparent)
                     {
                         worksheet.Cells[Row + row + 1, Column + col].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
-                        worksheet.Cells[Row + row + 1, Column + col].Style.Fill.BackgroundColor.SetColor(DataBackGroundColor);
+                        worksheet.Cells[Row + row + 1, Column + col].Style.Fill.BackgroundColor.SetColor(dataBackGroundColor);
                     }
-                    
+
                     worksheet.Cells[Row + row + 1, Column + col].Value = Data.Rows[row][col];
                 }
+            }
+        }
+        private void SetHeader(ExcelWorksheet worksheet)
+        {
+            for (int col = 0; col < Data.Columns.Count; col++)
+            {
+                worksheet.Cells[Row, Column + col].Style.Font.Bold = headerIsBold;
+                worksheet.Cells[Row, Column + col].Style.Font.Size = headerFontSize;
+                worksheet.Cells[Row, Column + col].Style.Font.Color.SetColor(headerFontColor);
+
+                if (headerBackGroundColor != Color.Transparent)
+                {
+                    worksheet.Cells[Row, Column + col].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+                    worksheet.Cells[Row, Column + col].Style.Fill.BackgroundColor.SetColor(headerBackGroundColor);
+                }
+
+                if (headers.Length >= col + 1)
+                    worksheet.Cells[Row, Column + col].Value = headers[col];
+                else
+                    worksheet.Cells[Row, Column + col].Value = Data.Columns[col].ColumnName;
             }
         }
         #endregion
