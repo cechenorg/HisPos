e7d72ab7eb4b0a6be2e175ed6835fcabdbe66f83
世彬 洪 <touycvbn95147@gmail.com>
2019-07-04 21:39:39 +0800

    yo reg pres okk

---
+++ NewClass/Product/ProductManagement/ProductRegisterPrescription.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace His_Pos.NewClass.Product.ProductManagement
+{
+    public struct ProductRegisterPrescription
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime? Date { get; set; }
+        public double Amount { get; set; }
+
+        public ProductRegisterPrescription(DataRow row)
+        {
+            ID = row.Field<int>("PreMas_ID");
+            Name = row.Field<string>("Cus_Name");
+            Date = row.Field<DateTime>("PreMas_AdjustDate");
+            Amount = row.Field<double>("PreDet_TotalAmount");
+        }
+    }
+}
