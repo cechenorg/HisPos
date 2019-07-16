dbd601a32f1c99a2188341a9e9460b54648ca12e
gokudera3306 <touycvbn95147@gmail.com>
2019-07-16 18:54:12 +0800

    uiiiiiiii yo

--- SYSTEM_TAB/INDEX/ReserveSendConfirmWindow/ReserveSendConfirmWindow.xaml.cs
+++ SYSTEM_TAB/INDEX/ReserveSendConfirmWindow/ReserveSendConfirmWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using His_Pos.NewClass.Prescription.IndexReserve.IndexReserveDetail;
+using His_Pos.SYSTEM_TAB.H2_STOCK_MANAGE.ProductManagement.ProductDetail;
 
 namespace His_Pos.SYSTEM_TAB.INDEX.ReserveSendConfirmWindow
 {
@@ -32,6 +34,17 @@ namespace His_Pos.SYSTEM_TAB.INDEX.ReserveSendConfirmWindow
                     Close();
             });
             ShowDialog();
+        }
+
+        private void ShowDetail(object sender, MouseButtonEventArgs e)
+        {
+            DataGridCell cell = sender as DataGridCell;
+
+            if (!(cell?.DataContext is IndexReserveDetail)) return;
+
+            ProductDetailWindow.ShowProductDetailWindow();
+
+            Messenger.Default.Send(new NotificationMessage<string[]>(this, new[] { ((IndexReserveDetail)cell.DataContext).ID, "0" }, "ShowProductDetail"));
         }
     }
 }
