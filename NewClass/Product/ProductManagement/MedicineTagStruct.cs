2a9b136d0dfe58460cb1da0a4a7442e12d918cbd
世彬 洪 <touycvbn95147@gmail.com>
2019-07-19 00:47:31 +0800

    yo finish

--- NewClass/Product/ProductManagement/MedicineTagStruct.cs
+++ NewClass/Product/ProductManagement/MedicineTagStruct.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic;
 
 namespace His_Pos.NewClass.Product.ProductManagement
 {
@@ -10,7 +12,7 @@ namespace His_Pos.NewClass.Product.ProductManagement
     {
         #region ----- Define Variables -----
         public string ID { get; set; }
-        public string FirstLetter { get{ return EnglishName.Length > 1 ? EnglishName.Substring(0,1) : ""; } }
+        public string FirstLetter { get; set; }
         public string ChineseName { get; set; }
         public string EnglishName { get; set; }
         public bool IsControl { get; set; }
@@ -21,9 +23,24 @@ namespace His_Pos.NewClass.Product.ProductManagement
 
         public MedicineTagStruct(string iD, string chineseName, string englishName, bool isControl, int? controlLevel, bool isFrozen, string ingredient)
         {
+            var temp = Strings.StrConv(englishName, VbStrConv.Narrow, 0).Replace("\"", "");
+
+            Regex regex = new Regex("[a-zA-Z-]*");
+            Match match = regex.Match(temp);
+
+            if (match.Groups.Count > 0)
+            {
+                FirstLetter = match.Groups[0].Value.Length > 1 ? match.Groups[0].Value.Substring(0, 1).ToUpper() : "";
+                EnglishName = match.Groups[0].Value.Length > 1 ? match.Groups[0].Value.Substring(1) : "";
+            }
+            else
+            {
+                FirstLetter = "";
+                EnglishName = "";
+            }
+
             ID = iD;
-            ChineseName = chineseName;
-            EnglishName = englishName.Length > 1 ? englishName.Substring(1) : "";
+            ChineseName = Strings.StrConv(chineseName, VbStrConv.Narrow, 0).Replace("\"", "");
             IsControl = isControl;
             ControlLevel = controlLevel;
             IsFrozen = isFrozen;
