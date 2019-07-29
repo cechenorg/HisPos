5cb943ccac9edd21ed3fbd6c0359a75e61476f9b
gokudera3306 <touycvbn95147@gmail.com>
2019-07-29 21:28:51 +0800

    12

--- NewClass/Medicine/Base/Medicines.cs
+++ NewClass/Medicine/Base/Medicines.cs
@@ -404,6 +404,7 @@ namespace His_Pos.NewClass.Medicine.Base
         private List<string> CreateIdList()
         {
             var idList = new List<string>();
+
             foreach (var m in Items)
             {
                 switch (m)
