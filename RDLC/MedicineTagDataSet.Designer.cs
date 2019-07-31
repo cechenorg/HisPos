3bb04791c3198891af23386233b2dcf921ebebd1
Frank <fk035372150@gmail.com>
2019-07-31 11:52:07 +0800

    OK

--- RDLC/MedicineTagDataSet.Designer.cs
+++ RDLC/MedicineTagDataSet.Designer.cs
@@ -287,8 +287,6 @@ namespace His_Pos.RDLC {
             
             private global::System.Data.DataColumn columnIngredient;
             
-            private global::System.Data.DataColumn columnIsFrozen;
-            
             private global::System.Data.DataColumn columnIsControl;
             
             private global::System.Data.DataColumn columnID;
@@ -364,14 +362,6 @@ namespace His_Pos.RDLC {
             
             [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
             [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "16.0.0.0")]
-            public global::System.Data.DataColumn IsFrozenColumn {
-                get {
-                    return this.columnIsFrozen;
-                }
-            }
-            
-            [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-            [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "16.0.0.0")]
             public global::System.Data.DataColumn IsControlColumn {
                 get {
                     return this.columnIsControl;
@@ -439,14 +429,13 @@ namespace His_Pos.RDLC {
             
             [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
             [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "16.0.0.0")]
-            public MedicineTagRow AddMedicineTagRow(string FirstLetter, string EnglishName, string ChineseName, string Ingredient, bool IsFrozen, bool IsControl, string ID, string ControlLevel, string Unit) {
+            public MedicineTagRow AddMedicineTagRow(string FirstLetter, string EnglishName, string ChineseName, string Ingredient, bool IsControl, string ID, string ControlLevel, string Unit) {
                 MedicineTagRow rowMedicineTagRow = ((MedicineTagRow)(this.NewRow()));
                 object[] columnValuesArray = new object[] {
                         FirstLetter,
                         EnglishName,
                         ChineseName,
                         Ingredient,
-                        IsFrozen,
                         IsControl,
                         ID,
                         ControlLevel,
@@ -477,7 +466,6 @@ namespace His_Pos.RDLC {
                 this.columnEnglishName = base.Columns["EnglishName"];
                 this.columnChineseName = base.Columns["ChineseName"];
                 this.columnIngredient = base.Columns["Ingredient"];
-                this.columnIsFrozen = base.Columns["IsFrozen"];
                 this.columnIsControl = base.Columns["IsControl"];
                 this.columnID = base.Columns["ID"];
                 this.columnControlLevel = base.Columns["ControlLevel"];
@@ -495,8 +483,6 @@ namespace His_Pos.RDLC {
                 base.Columns.Add(this.columnChineseName);
                 this.columnIngredient = new global::System.Data.DataColumn("Ingredient", typeof(string), null, global::System.Data.MappingType.Element);
                 base.Columns.Add(this.columnIngredient);
-                this.columnIsFrozen = new global::System.Data.DataColumn("IsFrozen", typeof(bool), null, global::System.Data.MappingType.Element);
-                base.Columns.Add(this.columnIsFrozen);
                 this.columnIsControl = new global::System.Data.DataColumn("IsControl", typeof(bool), null, global::System.Data.MappingType.Element);
                 base.Columns.Add(this.columnIsControl);
                 this.columnID = new global::System.Data.DataColumn("ID", typeof(string), null, global::System.Data.MappingType.Element);
@@ -505,8 +491,6 @@ namespace His_Pos.RDLC {
                 base.Columns.Add(this.columnControlLevel);
                 this.columnUnit = new global::System.Data.DataColumn("Unit", typeof(string), null, global::System.Data.MappingType.Element);
                 base.Columns.Add(this.columnUnit);
-                this.columnIsFrozen.AllowDBNull = false;
-                this.columnIsFrozen.DefaultValue = ((bool)(false));
                 this.columnIsControl.AllowDBNull = false;
                 this.columnIsControl.DefaultValue = ((bool)(false));
             }
@@ -710,17 +694,6 @@ namespace His_Pos.RDLC {
                 }
                 set {
                     this[this.tableMedicineTag.IngredientColumn] = value;
-                }
-            }
-            
-            [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-            [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "16.0.0.0")]
-            public bool IsFrozen {
-                get {
-                    return ((bool)(this[this.tableMedicineTag.IsFrozenColumn]));
-                }
-                set {
-                    this[this.tableMedicineTag.IsFrozenColumn] = value;
                 }
             }
             
