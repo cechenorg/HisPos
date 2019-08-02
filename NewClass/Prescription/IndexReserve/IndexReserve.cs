64c4c9545364f0d4bcd6c804697f27ca0f0c2b1d
Frank <fk035372150@gmail.com>
2019-08-02 15:03:58 +0800

    封包明細 期限少一天

--- NewClass/Prescription/IndexReserve/IndexReserve.cs
+++ NewClass/Prescription/IndexReserve/IndexReserve.cs
@@ -216,7 +216,7 @@ namespace His_Pos.NewClass.Prescription.IndexReserve
                 new ReportParameter("PatientTel",PhoneNote),
                 new ReportParameter("Institution", InsName),
                 new ReportParameter("Division", DivName),
-                new ReportParameter("AdjustRange", $"{AdjustDate.AddYears(-1911).ToString("yyy-MM-dd")} ~ {AdjustDate.AddYears(-1911).AddDays(20).ToString("yyy-MM-dd")}")
+                new ReportParameter("AdjustRange", $"{AdjustDate.AddYears(-1911).ToString("yyy-MM-dd")} ~ {AdjustDate.AddYears(-1911).AddDays(19).ToString("yyy-MM-dd")}")
             };
         }
 
