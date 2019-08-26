1f74b8efc300f556a5241fa5c6ac23ce2c232a87
xilian <user@MSI>
2019-08-26 10:18:51 +0800

    cus

--- SYSTEM_TAB/H4_BASIC_MANAGE/CustomerManage/CustomerManageViewModel.cs
+++ SYSTEM_TAB/H4_BASIC_MANAGE/CustomerManage/CustomerManageViewModel.cs
@@ -68,6 +68,12 @@ namespace His_Pos.SYSTEM_TAB.H4_BASIC_MANAGE.CustomerManage {
             get { return textCusBirthDay; }
             set { Set(() => TextCusBirthDay, ref textCusBirthDay, value); }
         }
+        public bool isDataChanged;
+        public bool IsDataChanged
+        {
+            get { return isDataChanged; }
+            set { Set(() => IsDataChanged, ref isDataChanged, value); }
+        }
         public bool btnCancelEnable;
         public bool BtnCancelEnable
         {
@@ -230,6 +236,7 @@ namespace His_Pos.SYSTEM_TAB.H4_BASIC_MANAGE.CustomerManage {
             Customer.Save();
             MainWindow.ServerConnection.CloseConnection();
             InitDataChanged();
+            MessageWindow.ShowMessage("更新成功!",Class.MessageType.SUCCESS);
         }
         private void ShowMedicinesDetailAction()
         {
@@ -241,17 +248,15 @@ namespace His_Pos.SYSTEM_TAB.H4_BASIC_MANAGE.CustomerManage {
         #region Function 
         private void DataChanged()
         {
-              
-            ChangeText = "已修改";
-            ChangeForeground = "Red"; 
+
+            IsDataChanged = true; 
             BtnCancelEnable = true;
             BtnSubmitEnable = true;
         }
 
         private void InitDataChanged()
         {
-            ChangeText = "未修改";
-            ChangeForeground = "Black";
+            IsDataChanged = false;
             BtnCancelEnable = false;
             BtnSubmitEnable = false;
         }
