6ad0fa0a8da9f8acf304abbdab2d839440e8c2e9
gokudera3306 <touycvbn95147@gmail.com>
2019-09-03 17:18:03 +0800

    on shelf

--- NewClass/Product/ProductManagement/ProductManageStructs.cs
+++ NewClass/Product/ProductManagement/ProductManageStructs.cs
@@ -7,17 +7,12 @@ namespace His_Pos.NewClass.Product.ProductManagement
 {
     public class ProductManageStructs : ObservableCollection<ProductManageStruct>
     {
-        public double TotalStockValueInRange { get; set; } = 0;
-
         public ProductManageStructs(DataTable dataTable)
         {
             foreach (DataRow row in dataTable.Rows)
             {
                 Add(new ProductManageStruct(row));
             }
-
-            if(dataTable.Rows.Count > 0)
-                TotalStockValueInRange = (double)dataTable.Rows[0].Field<decimal>("SELECT_RANGE_STOCK");
         }
 
         internal static ProductManageStructs SearchProductByConditions(string searchID, string searchName, bool searchIsEnable, bool searchIsInventoryZero, string wareID)
