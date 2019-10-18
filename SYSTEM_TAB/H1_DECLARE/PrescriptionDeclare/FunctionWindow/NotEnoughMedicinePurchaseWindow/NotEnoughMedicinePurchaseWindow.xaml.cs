a530326fcd53ef810fef960da5178f6c38e50b5d
s1021430 <s1021430@mail.yzu.edu.tw>
2019-10-18 17:30:16 +0800

    OK

--- SYSTEM_TAB/H1_DECLARE/PrescriptionDeclare/FunctionWindow/NotEnoughMedicinePurchaseWindow/NotEnoughMedicinePurchaseWindow.xaml.cs
+++ SYSTEM_TAB/H1_DECLARE/PrescriptionDeclare/FunctionWindow/NotEnoughMedicinePurchaseWindow/NotEnoughMedicinePurchaseWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using GalaSoft.MvvmLight.Messaging;
 using His_Pos.NewClass.Medicine.InventoryMedicineStruct;
+using His_Pos.NewClass.Medicine.NotEnoughMedicine;
 
 namespace His_Pos.SYSTEM_TAB.H1_DECLARE.PrescriptionDeclare.FunctionWindow.NotEnoughMedicinePurchaseWindow
 {
@@ -26,7 +27,7 @@ namespace His_Pos.SYSTEM_TAB.H1_DECLARE.PrescriptionDeclare.FunctionWindow.NotEn
             InitializeComponent();
         }
 
-        public NotEnoughMedicinePurchaseWindow(string wareID,string note,NotEnoughMedicineStructs purchaseList)
+        public NotEnoughMedicinePurchaseWindow(string wareID,string note,NotEnoughMedicines purchaseList)
         {
             InitializeComponent();
             DataContext = new NotEnoughMedicinePurchaseViewModel(wareID,note,purchaseList);
@@ -42,11 +43,24 @@ namespace His_Pos.SYSTEM_TAB.H1_DECLARE.PrescriptionDeclare.FunctionWindow.NotEn
 
         private void ShowMedicineDetail(object sender, MouseButtonEventArgs e)
         {
-            if (!(sender is DataGridCell cell) || !(cell.DataContext is NotEnoughMedicineStruct med)) return;
+            if (!(sender is DataGridCell cell) || !(cell.DataContext is NotEnoughMedicine med)) return;
             ((PrescriptionDeclareViewModel)DataContext).ShowMedicineDetail.Execute(med.ID);
         }
 
-        private void MedicineID_OnKeyDown(object sender, KeyEventArgs e)
+        private void DoubleTextBox_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            var t = sender as TextBox;
+            if (e.Key != Key.Decimal) return;
+            e.Handled = true;
+            if (t != null) t.CaretIndex++;
+        }
+
+        private void InputTextBox_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void InputTextBox_OnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             throw new NotImplementedException();
         }
