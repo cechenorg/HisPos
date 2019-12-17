3acef38da4de542824394b0b29105cbc50a10463
s1021430 <s1021430@mail.yzu.edu.tw>
2019-12-17 10:30:19 +0800

    incomeStatement not ok

---
+++ NewClass/Report/IncomeStatement/IncomeStatement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace His_Pos.NewClass.Report.IncomeStatement
+{
+    public class IncomeStatement
+    {
+        public IncomeStatement()
+        {
+
+        }
+    }
+}
---
+++ NewClass/Report/IncomeStatement/IncomeStatementMatrix.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace His_Pos.NewClass.Report.IncomeStatement
+{
+    public class IncomeStatementMatrix : MatrixLib.Matrix.MatrixBase<string,IncomeStatement>
+    {
+        public IncomeStatementMatrix()
+        {
+
+        }
+
+        public IncomeStatementMatrix(int year)
+        {
+            //var t = new DataTable();
+            //incomeStatement = new IncomeStatement[12];
+            //for (var i = 0; i < incomeStatement.Length; i++)
+            //    incomeStatement[i] = new IncomeStatement();
+            //foreach (var VARIABLE in COLLECTION)
+            //{
+                
+            //}
+        }
+
+        protected override IEnumerable<IncomeStatement> GetColumnHeaderValues()
+        {
+            return incomeStatement;
+        }
+
+        protected override IEnumerable<string> GetRowHeaderValues()
+        {
+            return rowHeaderToValueProviderMap.Keys;
+        }
+
+        protected override object GetCellValue(string rowHeaderValue, IncomeStatement columnHeaderValue)
+        {
+            return rowHeaderToValueProviderMap[rowHeaderValue](columnHeaderValue);
+        }
+
+        #region Fields
+
+        readonly IncomeStatement[] incomeStatement;
+        readonly Dictionary<string, CellValueProvider> rowHeaderToValueProviderMap;
+        private delegate object CellValueProvider(IncomeStatement incomeStatement);
+
+        #endregion
+    }
+}
