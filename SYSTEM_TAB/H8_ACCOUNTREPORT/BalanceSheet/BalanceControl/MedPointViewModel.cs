0056658303d367b0170fe4c892f151867ad57ea1
gokudera3306 <touycvbn95147@gmail.com>
2019-12-24 10:59:25 +0800

    add balance sheet files

---
+++ SYSTEM_TAB/H8_ACCOUNTREPORT/BalanceSheet/BalanceControl/MedPointViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace His_Pos.SYSTEM_TAB.H8_ACCOUNTREPORT.BalanceSheet.BalanceControl
+{
+    class MedPointViewModel
+    {
+    }
+}
--- SYSTEM_TAB/H8_ACCOUNTREPORT/BalanceSheet/BalanceSheetViewModel.cs
+++ SYSTEM_TAB/H8_ACCOUNTREPORT/BalanceSheet/BalanceSheetViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Command;
 using His_Pos.ChromeTabViewModel;
 
 namespace His_Pos.SYSTEM_TAB.H8_ACCOUNTREPORT.BalanceSheet
@@ -13,5 +14,18 @@ namespace His_Pos.SYSTEM_TAB.H8_ACCOUNTREPORT.BalanceSheet
         {
             return this;
         }
+
+        #region ----- Define Commands -----
+        public RelayCommand<string> ChangeDetailCommand { get; set; }
+        #endregion
+
+        #region ----- Define Variables -----
+        #endregion
+
+        #region ----- Define Actions -----
+        #endregion
+
+        #region ----- Define Functions -----
+        #endregion
     }
 }
