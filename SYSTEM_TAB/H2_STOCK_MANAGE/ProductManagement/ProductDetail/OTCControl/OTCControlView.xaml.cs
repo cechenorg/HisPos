93a7a15bbb65973f49530bdab02ab96c7679ecae
cechenapp <52332328+cechenapp@users.noreply.github.com>
2020-04-06 16:18:59 +0800

    Update OTCControlView.xaml.cs

--- SYSTEM_TAB/H2_STOCK_MANAGE/ProductManagement/ProductDetail/OTCControl/OTCControlView.xaml.cs
+++ SYSTEM_TAB/H2_STOCK_MANAGE/ProductManagement/ProductDetail/OTCControl/OTCControlView.xaml.cs
@@ -1,4 +1,12 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Messaging;
+using His_Pos.ChromeTabViewModel;
+using His_Pos.Class;
+using His_Pos.FunctionWindow;
+using His_Pos.NewClass.Product.ProductManagement;
+using His_Pos.SYSTEM_TAB.H2_STOCK_MANAGE.ProductPurchaseRecord;
 
 namespace His_Pos.SYSTEM_TAB.H2_STOCK_MANAGE.ProductManagement.ProductDetail.OTCControl
 {
@@ -10,6 +18,35 @@ namespace His_Pos.SYSTEM_TAB.H2_STOCK_MANAGE.ProductManagement.ProductDetail.OTC
         public OTCControlView()
         {
             InitializeComponent();
+        }
+
+        private void IsCommon_OnClick(object sender, RoutedEventArgs e)
+        {
+            CheckBox checkBox = sender as CheckBox;
+
+            if (checkBox is null) return;
+
+            if ((bool)checkBox.IsChecked)
+            {
+                MessageWindow.ShowMessage("架上安全量: 架上量小於安全量時，會在常備採購時轉出採購\r\n\r\n基準量: 架上量小於安全量時，會採購至基準量\r\n\r\n包裝量: 常備採購時，會採購包裝量的倍數", MessageType.TIPS);
+            }
+        }
+        private void TextBox_OnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            if (textBox is null) return;
+
+            textBox.Focus();
+        }
+
+        private void TextBox_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            if (textBox is null) return;
+
+            textBox.SelectAll();
         }
     }
 }
