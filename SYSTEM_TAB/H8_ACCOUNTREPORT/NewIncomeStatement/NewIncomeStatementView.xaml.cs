a5302bd3fb3eb2c21b569859948ce807c4fcbd07
cechenapp <52332328+cechenapp@users.noreply.github.com>
2020-12-28 23:07:42 +0800

    1228 IS_adjust

--- SYSTEM_TAB/H8_ACCOUNTREPORT/NewIncomeStatement/NewIncomeStatementView.xaml.cs
+++ SYSTEM_TAB/H8_ACCOUNTREPORT/NewIncomeStatement/NewIncomeStatementView.xaml.cs
@@ -47,9 +47,18 @@ namespace His_Pos.SYSTEM_TAB.H8_ACCOUNTREPORT.NewIncomeStatement
         {
             int.TryParse(tbYear.Text, out year);
 
-            bi.IsBusy = true;
-            if (!bgWorker.IsBusy)
-                bgWorker.RunWorkerAsync();
+            if (year > 2000 && year < 2100)
+            {
+                bi.IsBusy = true;
+                if (!bgWorker.IsBusy)
+                {
+                    bgWorker.RunWorkerAsync();
+                }
+            }
+            else 
+            {
+                MessageWindow.ShowMessage("查詢範圍有誤", MessageType.ERROR);
+            }
         }
 
         private void GetData(int year) 
@@ -78,6 +87,41 @@ namespace His_Pos.SYSTEM_TAB.H8_ACCOUNTREPORT.NewIncomeStatement
             }));
 
             MainWindow.ServerConnection.CloseConnection();
+
+            DataTable total = income.Clone();
+
+            if ((income.Rows.Count > 0) && (expanse.Rows.Count > 0) && (closed.Rows.Count > 0))
+            {
+                total.Rows.Add(income.Rows[income.Rows.Count - 1].ItemArray);
+                total.Rows[0][0] = "營業毛利";
+                total.Rows.Add(expanse.Rows[expanse.Rows.Count - 1].ItemArray);
+                total.Rows[1][0] = "費用";
+                total.Rows.Add(closed.Rows[closed.Rows.Count - 1].ItemArray);
+                total.Rows[2][0] = "結案差額";
+
+                DataRow totalRow = total.NewRow();
+                totalRow["MONTH"] = "總計";
+                totalRow["JAN"] = total.Compute("Sum(JAN)", string.Empty);
+                totalRow["FEB"] = total.Compute("Sum(FEB)", string.Empty);
+                totalRow["MAR"] = total.Compute("Sum(MAR)", string.Empty);
+                totalRow["APR"] = total.Compute("Sum(APR)", string.Empty);
+                totalRow["MAY"] = total.Compute("Sum(MAY)", string.Empty);
+                totalRow["JUN"] = total.Compute("Sum(JUN)", string.Empty);
+                totalRow["JUL"] = total.Compute("Sum(JUL)", string.Empty);
+                totalRow["AUG"] = total.Compute("Sum(AUG)", string.Empty);
+                totalRow["SEP"] = total.Compute("Sum(SEP)", string.Empty);
+                totalRow["OCT"] = total.Compute("Sum(OCT)", string.Empty);
+                totalRow["NOV"] = total.Compute("Sum(NOV)", string.Empty);
+                totalRow["DEC"] = total.Compute("Sum(DEC)", string.Empty);
+                totalRow["TOTAL"] = total.Compute("Sum(TOTAL)", string.Empty);
+                total.Rows.Add(totalRow);
+            }
+
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                dgTotal.ItemsSource = total.DefaultView;
+                dgTotal.Items.Refresh();
+            }));
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -88,6 +132,34 @@ namespace His_Pos.SYSTEM_TAB.H8_ACCOUNTREPORT.NewIncomeStatement
         private void dg_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
         {
             scrollviewer.ScrollToVerticalOffset(scrollviewer.VerticalOffset - e.Delta);
+        }
+
+        private void btnLeft_Click(object sender, RoutedEventArgs e)
+        {
+            year -= 1;
+            tbYear.Text = year.ToString();
+            InitData();
+        }
+
+        private void btnRight_Click(object sender, RoutedEventArgs e)
+        {
+            year += 1;
+            tbYear.Text = year.ToString();
+            InitData();
+        }
+
+        private void tbYear_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            tb.Dispatcher.BeginInvoke(new Action(() => tb.SelectAll()));
+        }
+
+        private void tbYear_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter) 
+            {
+                InitData();
+            }
         }
     }
 }
