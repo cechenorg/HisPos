df2287a167226bd51b51e5ef18c78eb2650cdf29
tai12369 <xdman12369@yahoo.com.tw>
2021-01-14 17:06:18 +0800

    1/14 介面調整

--- SYSTEM_TAB/H1_DECLARE/AdditionalCashFlowManage/AdditionalCashFlowManageViewModel.cs
+++ SYSTEM_TAB/H1_DECLARE/AdditionalCashFlowManage/AdditionalCashFlowManageViewModel.cs
@@ -188,7 +188,7 @@ namespace His_Pos.SYSTEM_TAB.H1_DECLARE.AdditionalCashFlowManage
             SelectedCashFlowAccount = CashFlowAccounts[0];
             SelectedBank = SelectedType[0];
             CashFlowRecords = new CashFlowRecords();
-            StartDate = DateTime.Today;
+            StartDate = DateTime.Now.AddDays(1 - DateTime.Now.Day);
             EndDate = DateTime.Today;
             SearchAction();
         }
