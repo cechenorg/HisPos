467cd48ebcf9eb96996c9f09b44bb5b690fa446d
tai12369 <xdman12369@yahoo.com.tw>
2021-06-02 11:59:19 +0800

    6/2 把發票閃退修正

--- SYSTEM_TAB/P1_TRANSACTION/ProductTransaction/ProductTransactionView.xaml.cs
+++ SYSTEM_TAB/P1_TRANSACTION/ProductTransaction/ProductTransactionView.xaml.cs
@@ -528,6 +528,11 @@ namespace His_Pos.SYSTEM_TAB.P1_TRANSACTION.ProductTransaction
                     DepositInsert();
                     if (Properties.Settings.Default.InvoiceCheck == "1")
                     {
+                        if (Properties.Settings.Default.InvoiceNumber.Length != 8)
+                        {
+                            MessageWindow.ShowMessage("請確認發票設定！", MessageType.ERROR);
+                            return;
+                        }
                         tbInvoiceNum.Content = Properties.Settings.Default.InvoiceNumberEng.ToString()+Properties.Settings.Default.InvoiceNumber.ToString();
                         InvoicePrint();
                         InvoiceControlViewModel vm = new InvoiceControlViewModel();
