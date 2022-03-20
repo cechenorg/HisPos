42c123af472f8af6955d35f10a99d35aad7564dc
PC <PC@DESKTOP-MCPU5FH>
2022-03-20 22:36:34 +0800

    [xilian] revert

---
+++ HISPOS/SYSTEM_TAB/H2_STOCK_MANAGE/LocationManage/InsertLocationDetailWindow.xaml.cs
@@ -0,0 +1,155 @@
+﻿using His_Pos.Class;
+using His_Pos.FunctionWindow;
+using His_Pos.FunctionWindow.AddProductWindow;
+using His_Pos.NewClass.Product;
+using His_Pos.NewClass.ProductLocation;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Windows.Input;
+
+namespace His_Pos.SYSTEM_TAB.H2_STOCK_MANAGE.LocationManage
+{
+    /// <summary>
+    /// AddTypeWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class InsertLocationDetailWindow : Window
+    {
+        private int ID;
+
+        public InsertLocationDetailWindow(int i)
+        {
+            InitializeComponent();
+            ID = i;
+            DataContext = this;
+        }
+
+        #region ----- Define Functions -----
+
+        private bool CheckEmptyData()
+        {
+            string error = "";
+
+            if (ProID.Text.Equals(""))
+                error += "未填寫名稱!\n";
+
+            if (error.Length != 0)
+            {
+                MessageWindow.ShowMessage(error, Class.MessageType.ERROR);
+
+                return false;
+            }
+
+            return true;
+        }
+
+        #endregion ----- Define Functions -----
+
+        #region ----- Define Events -----
+
+        private void ConfrimClick(object sender, RoutedEventArgs e)
+        {
+            if (CheckEmptyData())
+            {
+                MainWindow.ServerConnection.OpenConnection();
+                DataTable dataTable = ProductLocationDB.InsertProductLocationDetails(ID, ProID.Text);
+                MainWindow.ServerConnection.CloseConnection();
+
+                if (dataTable is null || dataTable.Rows.Count == 0)
+                {
+                    MessageWindow.ShowMessage("新增失敗 請稍後再試", Class.MessageType.ERROR);
+                    return;
+                }
+                {
+                    MessageWindow.ShowMessage("新增成功", Class.MessageType.SUCCESS);
+                }
+
+                Close();
+            }
+        }
+
+        #endregion ----- Define Events -----
+
+        private void ProID_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                AddProductByInputAction(ProID.Text);
+            }
+        }
+
+        private void AddProductByInputAction(string searchString)
+        {
+            if (string.IsNullOrEmpty(searchString)) return;
+            if (searchString.Length == 0)
+            {
+                return;
+            }
+
+            if (int.TryParse(searchString, out int n))
+            {
+                if (searchString.Length < 5)
+                {
+                    MessageWindow.ShowMessage("商品代碼長度不得小於5", MessageType.WARNING);
+                    ProID.Text = "";
+                    return;
+                }
+            }
+            else
+            {
+                if (searchString.Length < 2)
+                {
+                    MessageWindow.ShowMessage("搜尋字串長度不得小於2", MessageType.WARNING);
+                    ProID.Text = "";
+                    return;
+                }
+            }
+            MainWindow.ServerConnection.OpenConnection();
+            int productCount = ProductStructs.GetProductStructCountBySearchString(searchString, AddProductEnum.Trade);
+            MainWindow.ServerConnection.CloseConnection();
+
+            if (productCount == 0)
+            {
+                MessageWindow.ShowMessage("查無商品", MessageType.WARNING);
+                ProID.Text = "";
+                return;
+            }
+            else
+            {
+                if (productCount > 0)
+                {
+                    int WareID = 0;
+                    MainWindow.ServerConnection.OpenConnection();
+                    List<SqlParameter> parameters = new List<SqlParameter>();
+                    parameters.Add(new SqlParameter("SEARCH_STRING", searchString));
+                    parameters.Add(new SqlParameter("WAREHOUSE_ID", WareID));
+                    DataTable result = MainWindow.ServerConnection.ExecuteProc("[POS].[SearchProductsByID]", parameters);
+                    MainWindow.ServerConnection.CloseConnection();
+
+                    if (result.Rows.Count == 1)
+                    {
+                        ProID.Text = result.Rows[0]["Pro_ID"].ToString();
+                    }
+                    else if (result.Rows.Count > 1)
+                    {
+                        TradeAddProductWindow tapw = new TradeAddProductWindow(result);
+                        tapw.ShowDialog();
+                        DataRow NewProduct = tapw.SelectedProduct;
+                        int amt = 0;
+                        if (NewProduct != null)
+                        {
+                            ProID.Text = NewProduct["Pro_ID"].ToString();
+                        }
+                    }
+                }
+                else
+                {
+                    MessageWindow.ShowMessage("查無此商品", MessageType.WARNING);
+                    ProID.Text = "";
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ HISPOS/SYSTEM_TAB/H2_STOCK_MANAGE/LocationManage/ItemChangeWindow.xaml.cs
@@ -0,0 +1,284 @@
+﻿using His_Pos.Class;
+using His_Pos.FunctionWindow;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace His_Pos.SYSTEM_TAB.H2_STOCK_MANAGE.LocationManage
+{
+    /// <summary>
+    /// ItemChangeWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class ItemChangeWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        public class ChangeItem
+        {
+            public ChangeItem(string cid, string cname, string coldvalue, string cnewvalue)
+            {
+                id = cid;
+                name = cname;
+                oldvalue = coldvalue;
+                newvalue = cnewvalue;
+            }
+
+            public string id { get; set; }
+            public string name { get; set; }
+            public string oldvalue { get; set; }
+            public string newvalue { get; set; }
+        }
+
+        public class LocationbData
+        {
+            public LocationbData(DataRow row)
+            {
+                proid = row["PRO_ID"].ToString();
+                proname = row["PRO_NAME"].ToString();
+                locdName = row["LOCD_NAME"].ToString();
+            }
+
+            public string proid { get; set; }
+            public string proname { get; set; }
+            public string locdName { get; set; }
+        }
+
+        private ObservableCollection<ChangeItem> changeItems = new ObservableCollection<ChangeItem>();
+        private ObservableCollection<LocationbData> locationSourceDatas = new ObservableCollection<LocationbData>();
+
+        public ObservableCollection<LocationbData> LocationSourceDatas
+        {
+            get
+            {
+                return locationSourceDatas;
+            }
+            set
+            {
+                locationSourceDatas = value;
+                NotifyPropertyChanged("LocationSourceDatas");
+            }
+        }
+
+        private ObservableCollection<LocationbData> locationTargetDatas = new ObservableCollection<LocationbData>();
+
+        public ObservableCollection<LocationbData> LocationTargetDatas
+        {
+            get
+            {
+                return locationTargetDatas;
+            }
+            set
+            {
+                locationTargetDatas = value;
+                NotifyPropertyChanged("LocationTargetDatas");
+            }
+        }
+
+        private ObservableCollection<string> sourceBig = new ObservableCollection<string>();
+        private ObservableCollection<string> sourceSmall = new ObservableCollection<string>();
+
+        public ItemChangeWindow(string view)
+        {
+            InitializeComponent();
+            DataContext = this;
+            switch (view)
+            {
+                case "Location":
+                    ///ComboBoxSourceBig.ItemsSource = LocationDb.ObservableGetLocationData();
+                    ///ComboBoxSourceSmall.ItemsSource = LocationDb.ObservableGetLocationDetail();
+                    ///ComboBoxTargetBig.ItemsSource = LocationDb.ObservableGetLocationData();
+                    ///ComboBoxTargetSmall.ItemsSource = LocationDb.ObservableGetLocationDetail();
+                    ///foreach (DataRow row in LocationDb.GetProductLocation().Rows)
+                    ///{
+                    ///    LocationSourceDatas.Add(new LocationbData(row));
+                    ///    LocationTargetDatas.Add(new LocationbData(row));
+                    ///}
+                    DataGridTarget.Items.Filter = ProductLocationTargetFilter;
+                    DataGridSource.Items.Filter = ProductLocationSourceFilter;
+                    break;
+            }
+        }
+
+        private void ComboBoxSourceBig_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboBoxSourceBig.SelectedItem is null) return;
+            if (ComboBoxSourceBig.SelectedItem.ToString() == "尚未有櫃位產品")
+            {
+                DataGridSource.Items.Filter = LocationDetailEmptySourceFilter;
+            }
+            ComboBoxSourceSmall.Items.Filter = LocationDetailSourceFilter;
+        }
+
+        public bool LocationDetailEmptySourceFilter(object item)
+        {
+            if (((LocationbData)item).locdName == string.Empty)
+                return true;
+            else
+                return false;
+        }
+
+        private void ComboBoxSourceSmall_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if ((ComboBoxSourceBig.SelectedItem is null))
+                ComboBoxSourceBig.Text = ComboBoxSourceSmall.SelectedItem.ToString().Split('-')[0];
+            DataGridSource.Items.Filter = ProductLocationSourceFilter;
+        }
+
+        private void ComboBoxTargetBig_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboBoxTargetBig.SelectedItem is null) return;
+            if (ComboBoxTargetBig.SelectedItem.ToString() == "尚未有櫃位產品")
+            {
+                DataGridSource.Items.Filter = LocationDetailEmptySourceFilter;
+            }
+            ComboBoxTargetSmall.Items.Filter = LocationDetailTargetFilter;
+        }
+
+        public bool LocationDetailEmptyTargetFilter(object item)
+        {
+            if (((LocationbData)item).locdName == string.Empty)
+                return true;
+            else
+                return false;
+        }
+
+        private void ComboBoxTargetSmall_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if ((ComboBoxTargetBig.SelectedItem is null))
+                ComboBoxTargetBig.Text = ComboBoxTargetSmall.SelectedItem.ToString().Split('-')[0];
+            DataGridTarget.Items.Filter = ProductLocationTargetFilter;
+        }
+
+        private void ButtonPlus_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (DataGridSource.SelectedItem is null)
+            {
+                MessageWindow.ShowMessage("請選擇項目", MessageType.ERROR);
+
+                return;
+            }
+            if (ComboBoxSourceSmall.SelectedItem is null && ComboBoxSourceBig.SelectedItem.ToString() != "尚未有櫃位產品")
+            {
+                MessageWindow.ShowMessage("請選擇來源", MessageType.ERROR);
+
+                return;
+            }
+            if (ComboBoxTargetSmall.SelectedItem is null && ComboBoxTargetBig.SelectedItem.ToString() != "尚未有櫃位產品")
+            {
+                MessageWindow.ShowMessage("請選擇目的", MessageType.ERROR);
+
+                return;
+            }
+            var item = (DataGridSource.SelectedItem as LocationbData);
+            var goal = ComboBoxTargetSmall.SelectedItem == null ? "" : ComboBoxTargetSmall.SelectedItem;
+            foreach (LocationbData row in DataGridSource.SelectedItems)
+            {
+                changeItems.Add(new ChangeItem(item.proid, item.proname, item.locdName, goal.ToString()));
+                (DataGridSource.SelectedItem as LocationbData).locdName = goal.ToString();
+                LocationTargetDatas.Single(product => product.proid == (DataGridSource.SelectedItem as LocationbData).proid).locdName = goal.ToString();
+            }
+
+            if (goal.ToString() == "")
+                DataGridTarget.Items.Filter = LocationDetailEmptyTargetFilter;
+            else
+                DataGridTarget.Items.Filter = ProductLocationTargetFilter;
+
+            DataGridSource.Items.Filter = ProductLocationSourceFilter;
+
+            DataGridTarget.SelectedIndex = 0;
+            DataGridSource.SelectedIndex = 0;
+        }
+
+        private void ButtonBalance_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (DataGridTarget.SelectedItem is null)
+            {
+                MessageWindow.ShowMessage("請選擇項目", MessageType.ERROR);
+
+                return;
+            }
+            if (ComboBoxTargetSmall.SelectedItem is null && ComboBoxTargetBig.SelectedItem.ToString() != "尚未有櫃位產品")
+            {
+                MessageWindow.ShowMessage("請選擇來源", MessageType.ERROR);
+
+                return;
+            }
+            if (ComboBoxSourceSmall.SelectedItem is null && ComboBoxSourceBig.SelectedItem.ToString() != "尚未有櫃位產品")
+            {
+                MessageWindow.ShowMessage("請選擇目的", MessageType.ERROR);
+
+                return;
+            }
+            var item = (DataGridTarget.SelectedItem as LocationbData);
+            var goal = ComboBoxSourceSmall.SelectedItem == null ? "" : ComboBoxSourceSmall.SelectedItem;
+
+            foreach (LocationbData row in DataGridTarget.SelectedItems)
+            {
+                changeItems.Add(new ChangeItem(item.proid, item.proname, item.locdName, goal.ToString()));
+                (DataGridTarget.SelectedItem as LocationbData).locdName = goal.ToString();
+                LocationSourceDatas.Single(product => product.proid == (DataGridTarget.SelectedItem as LocationbData).proid).locdName = goal.ToString();
+            }
+
+            if (goal.ToString() == "")
+                DataGridSource.Items.Filter = LocationDetailEmptySourceFilter;
+            else
+                DataGridSource.Items.Filter = ProductLocationSourceFilter;
+
+            DataGridTarget.Items.Filter = ProductLocationTargetFilter;
+            DataGridTarget.SelectedIndex = 0;
+            DataGridSource.SelectedIndex = 0;
+        }
+
+        public bool ProductLocationSourceFilter(object item)
+        {
+            if (ComboBoxSourceSmall.SelectedItem is null) return false;
+            if (((LocationbData)item).locdName.Equals(ComboBoxSourceSmall.SelectedItem.ToString()))
+                return true;
+            else
+                return false;
+        }
+
+        public bool ProductLocationTargetFilter(object item)
+        {
+            if (ComboBoxTargetSmall.SelectedItem is null) return false;
+            if (((LocationbData)item).locdName.Equals(ComboBoxTargetSmall.SelectedItem.ToString()))
+                return true;
+            else
+                return false;
+        }
+
+        public bool LocationDetailSourceFilter(object item)
+        {
+            if (item.ToString().Split('-')[0].Equals(ComboBoxSourceBig.SelectedItem.ToString()))
+                return true;
+            else
+                return false;
+        }
+
+        public bool LocationDetailTargetFilter(object item)
+        {
+            if (item.ToString().Split('-')[0].Equals(ComboBoxTargetBig.SelectedItem.ToString()))
+                return true;
+            else
+                return false;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeResultWindow changeResultWindow = new ChangeResultWindow(changeItems);
+            changeResultWindow.ShowDialog();
+        }
+    }
+}
