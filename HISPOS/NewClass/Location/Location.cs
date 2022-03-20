e9b4c74567b91c5d0d9c4bd35b8e7d280ab24cbe
PC <PC@DESKTOP-MCPU5FH>
2022-03-20 22:53:46 +0800

    復原櫃位功能

---
+++ HISPOS/NewClass/Location/Location.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Data;
+
+namespace His_Pos.Class.Location
+{
+    public class Location
+    {
+        public Location(DataRow row)
+        {
+            id = row["LOC_ID"].ToString();
+            name = row["LOC_NAME"].ToString();
+            pathX = Convert.ToDouble(row["LOC_X"].ToString());
+            pathY = Convert.ToDouble(row["LOC_Y"].ToString());
+            width = Convert.ToDouble(row["LOC_WIDTH"].ToString());
+            heigh = Convert.ToDouble(row["LOC_HEIGHT"].ToString());
+        }
+
+        public Location(int locId, string locName, double locpathX, double locpathY, double locWidth, double locHeigh)
+        {
+            id = locId.ToString();
+            name = locName;
+            pathX = locpathX;
+            pathY = locpathY;
+            width = locWidth;
+            heigh = locHeigh;
+        }
+
+        public string id { get; }
+        public string name { get; }
+        public double pathX { get; }
+        public double pathY { get; }
+        public double width { get; }
+        public double heigh { get; }
+        public ObservableCollection<LocationDetail> locationDetailCollection = new ObservableCollection<LocationDetail>();
+    }
+}
