8d3081226ef413e0235d5a4f782c7a7a9e37c6d1
PC <PC@DESKTOP-MCPU5FH>
2022-05-06 11:58:45 +0800

    update UpdateClosingAccountTarget parameter

--- HISPOS/SYSTEM_TAB/H11_CLOSING/ClossingCashSelect/ClosingAccountReport/ClosingAccountReportRepository.cs
+++ HISPOS/SYSTEM_TAB/H11_CLOSING/ClossingCashSelect/ClosingAccountReport/ClosingAccountReportRepository.cs
@@ -90,7 +90,7 @@ namespace His_Pos.NewClass.AccountReport.ClosingAccountReport
             parameterList.Add(new SqlParameter("verifykey", data.VerifyKey));
             parameterList.Add(new SqlParameter("TargetMonth", data.Month));
             parameterList.Add(new SqlParameter("TargetValue", data.MonthlyTarget));
-            parameterList.Add(new SqlParameter("EmpID", ViewModelMainWindow.CurrentUser.ID));
+            parameterList.Add(new SqlParameter("EmpAccount", ViewModelMainWindow.CurrentUser.Account));
             
             MainWindow.ServerConnection.ExecuteProcBySchema(
                ViewModelMainWindow.CurrentPharmacy.GroupServerName, "[Set].[InsertUpdateAccountTarget]", parameterList);
