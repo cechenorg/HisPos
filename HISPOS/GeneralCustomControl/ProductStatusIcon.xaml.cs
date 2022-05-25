b45f6b1441a699074fc1ea439bdb4ad614bf3ec5
weilun593 <weilun593@gmail.com>
2022-05-25 11:56:25 +0800

    進退貨管理-總檔增加顯示特殊藥品

--- HISPOS/GeneralCustomControl/ProductStatusIcon.xaml.cs
+++ HISPOS/GeneralCustomControl/ProductStatusIcon.xaml.cs
@@ -36,6 +36,11 @@ namespace His_Pos.GeneralCustomControl
             get { return (bool)GetValue(IsFrozenProperty); }
             set { SetValue(IsFrozenProperty, value); }
         }
+        public bool IsSinControl //總檔用
+        {
+            get { return (bool)GetValue(IsSinControlProperty); }
+            set { SetValue(IsSinControlProperty, value); }
+        }
 
         public bool IsMerged
         {
@@ -67,6 +72,9 @@ namespace His_Pos.GeneralCustomControl
         public static readonly DependencyProperty IsFrozenProperty =
             DependencyProperty.Register("IsFrozen", typeof(bool), typeof(ProductStatusIcon), new PropertyMetadata(false));
 
+        public static readonly DependencyProperty IsSinControlProperty =
+            DependencyProperty.Register("IsSinControl", typeof(bool), typeof(ProductStatusIcon), new PropertyMetadata(false));
+
         public static readonly DependencyProperty IsIsMergedProperty =
             DependencyProperty.Register("IsMerged", typeof(bool), typeof(ProductStatusIcon), new PropertyMetadata(false));
 
@@ -87,7 +95,7 @@ namespace His_Pos.GeneralCustomControl
         #endregion ----- Define DependencyProperty -----
 
         #region ----- Define Variables -----
-
+        
         public bool IsControl { get { return ControlLevel > 0; } }
         public bool ShowSafeAmount { get { return CommonSafeAmount > 0; } }
 
--- HISPOS/NewClass/StoreOrder/SingdeTotalOrder/ProcessingStoreOrder.cs
+++ HISPOS/NewClass/StoreOrder/SingdeTotalOrder/ProcessingStoreOrder.cs
@@ -14,6 +14,8 @@ namespace His_Pos.NewClass.StoreOrder.SingdeTotalOrder
         public string RecID { get; set; }
         public string CheckCode { get; set; }
         public string IsType { get; set; }//藥品、OTC
+        public int IsControl { get; set; }//訂單是否有管制藥
+        public int IsFrozen { get; set; }//訂單是否有冰品
         public OrderTypeEnum Type { get; set; }
 
         public OrderStatusEnum Status
@@ -37,6 +39,8 @@ namespace His_Pos.NewClass.StoreOrder.SingdeTotalOrder
             Status = row.Field<string>("StoOrd_Status").Equals("D") ? OrderStatusEnum.DONE : OrderStatusEnum.SINGDE_PROCESSING;
             Total = (double)row.Field<decimal>("TOTAL");
             Note = row.Field<string>("StoOrd_Note");
+            IsControl = row.Field<int>("IsControl");
+            IsFrozen = row.Field<int>("IsFrozen");
         }
     }
 }
