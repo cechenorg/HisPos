a473901bcd4c39bf6974d55e0c6d5aaab0f17c2e
PC <PC@DESKTOP-MCPU5FH>
2022-06-06 21:41:05 +0800

    add Authority enum

---
+++ DomainModel/Enum/Authority.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel.Enum
+{
+    public enum Authority
+    {
+        系統管理員 = 1,
+        店長 = 2,
+        店員 = 3,
+        藥師 = 4 
+    }
+}
