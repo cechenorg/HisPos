9e188bc80f711d3f254baa8ef66fc7d4fcd7189f
weilun593 <weilun593@gmail.com>
2022-06-09 11:29:21 +0800

    進退貨紀錄-雙擊預設今天日期

--- HISPOS/SYSTEM_TAB/H2_STOCK_MANAGE/ProductPurchaseRecord/ProductPurchaseRecordView.xaml.cs
+++ HISPOS/SYSTEM_TAB/H2_STOCK_MANAGE/ProductPurchaseRecord/ProductPurchaseRecordView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
 using System.Windows.Input;
 using Xceed.Wpf.Toolkit;
 
@@ -24,6 +26,31 @@ namespace His_Pos.SYSTEM_TAB.H2_STOCK_MANAGE.ProductPurchaseRecord
             {
                 EndDate.Focus();
                 EndDate.Select(0, 0);
+            }
+        }
+
+        private void StartDate_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            PresetToday(sender);
+        }
+
+        private void EndDate_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            PresetToday(sender);
+        }
+        private void PresetToday(object sender)
+        {
+            MaskedTextBox maskedTextBox = sender as MaskedTextBox;
+            if (maskedTextBox is null) return;
+            if (maskedTextBox.Text == "---/--/--")
+            {
+                DateTime dt = DateTime.Today;
+                TaiwanCalendar tc = new TaiwanCalendar();
+                string year = tc.GetYear(dt).ToString().PadLeft(3, '0');
+                string month = tc.GetMonth(dt).ToString().PadLeft(2, '0');
+                string day = tc.GetDayOfMonth(dt).ToString().PadLeft(2, '0');
+                string today = string.Format("{0}/{1}/{2}", year, month, day);
+                maskedTextBox.Text = today;
             }
         }
     }
