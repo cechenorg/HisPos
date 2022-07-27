3e9a5042e3ebc14e9fe44e2233b7387bafd7ee6a
weilun593 <weilun593@gmail.com>
2022-07-27 11:17:03 +0800

    杏德已出貨調整

--- HISPOS/SYSTEM_TAB/H2_STOCK_MANAGE/ProductPurchaseReturn/ProductPurchaseReturnViewModel.cs
+++ HISPOS/SYSTEM_TAB/H2_STOCK_MANAGE/ProductPurchaseReturn/ProductPurchaseReturnViewModel.cs
@@ -149,7 +149,8 @@ namespace His_Pos.SYSTEM_TAB.H2_STOCK_MANAGE.ProductPurchaseReturn
                             if (drs != null && drs.Length > 0)
                             {
                                 int flag = Convert.ToInt32(drs[0]["FLAG"]);
-                                if(flag == 2)
+                                string ReceiveID = Convert.ToString(drs[0]["PRESCRIPTION_RECEIVEID"]);
+                                if (flag == 2)
                                 {
                                     string VoidReason = "5.其他:杏德訂單已作廢";
                                     StoreOrderDB.RemoveStoreOrderByID(storeOrders[i].ID, VoidReason);
@@ -158,18 +159,21 @@ namespace His_Pos.SYSTEM_TAB.H2_STOCK_MANAGE.ProductPurchaseReturn
                                 }
                                 else
                                 {
-                                    if (storeOrders[i].ID == storeOrders[i].ReceiveID)//尚未更新為杏德單號
+                                    if(!string.IsNullOrEmpty(ReceiveID) && ReceiveID != null)
                                     {
-                                        storeOrders[i].OrderType = OrderTypeEnum.PREPARE;//已出貨
-                                        storeOrders[i].IsWaitOrder = 0;
+                                        if (storeOrders[i].ID == storeOrders[i].ReceiveID)//尚未更新為杏德單號
+                                        {
+                                            storeOrders[i].OrderType = OrderTypeEnum.PREPARE;//已出貨
+                                            storeOrders[i].IsWaitOrder = 0;
+                                        }
+                                        else if (storeOrders[i].ID != storeOrders[i].ReceiveID)//已更新為杏德單號
+                                        {
+                                            storeOrders[i].OrderType = OrderTypeEnum.WAITPREPARE;//待入庫
+                                            storeOrders[i].IsWaitOrder = 0;
+                                        }
+                                        currentStoreOrder = storeOrders[i];
+                                        currentStoreOrder.UpdateOrderDataFromSingde(drs[0]);
                                     }
-                                    else if (storeOrders[i].ID != storeOrders[i].ReceiveID)//已更新為杏德單號
-                                    {
-                                        storeOrders[i].OrderType = OrderTypeEnum.WAITPREPARE;//待入庫
-                                        storeOrders[i].IsWaitOrder = 0;
-                                    }
-                                    currentStoreOrder = storeOrders[i];
-                                    currentStoreOrder.UpdateOrderDataFromSingde(drs[0]);
                                 }
                             }
                         }
