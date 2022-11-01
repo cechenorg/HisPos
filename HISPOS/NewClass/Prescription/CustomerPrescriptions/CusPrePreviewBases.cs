ee5a835d00b3b9aaf96508334b5ffe02fa60f324
xilian <xilian@DESKTOP-38FCB7V>
2022-11-01 23:39:41 +0800

    帶出處方 orderby DESC

--- HISPOS/NewClass/Prescription/CustomerPrescriptions/CusPrePreviewBases.cs
+++ HISPOS/NewClass/Prescription/CustomerPrescriptions/CusPrePreviewBases.cs
@@ -2,12 +2,15 @@
 using His_Pos.NewClass.Cooperative.XmlOfPrescription;
 using His_Pos.Service;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data;
+using System.Linq;
 using System.Xml;
 using His_Pos.Class;
 using His_Pos.FunctionWindow;
+using System.Windows.Shell;
 
 namespace His_Pos.NewClass.Prescription.CustomerPrescriptions
 {
@@ -62,45 +65,76 @@ namespace His_Pos.NewClass.Prescription.CustomerPrescriptions
             }
                
             Clear();
+
+            List<CooperativePreview> tempList = new List<CooperativePreview>();
             var table = PrescriptionDb.GetXmlOfPrescriptionsByCusIDNumber(idNumber);
             foreach (DataRow r in table.Rows)
             {
                 var xDocument = new XmlDocument();
                 xDocument.LoadXml(r["CooCli_XML"].ToString());
-                Add(new CooperativePreview(XmlService.Deserialize<CooperativePrescription.Prescription>(xDocument.InnerXml)
+                tempList.Add(new CooperativePreview(XmlService.Deserialize<CooperativePrescription.Prescription>(xDocument.InnerXml)
                     , r.Field<DateTime>("CooCli_InsertTime"), r.Field<int>("CooCli_ID").ToString(), r.Field<bool>("CooCli_IsRead")));
             }
+
+            foreach (var data in tempList.OrderByDescending(_ => _.AdjustDate))
+            {
+                Add(data);
+            }
+
             GetOrthopedicsByCustomerIDNumber(idNumber);
         }
 
         public void GetRegisterByCusId(int cusID) //取得登錄慢箋
         {
             Clear();
+
+            List<ChronicPreview> tempList = new List<ChronicPreview>();
             var table = PrescriptionDb.GetRegisterPrescriptionByCusId(cusID);
             foreach (DataRow r in table.Rows)
             {
-                Add(new ChronicPreview(r, PrescriptionType.ChronicRegister));
+                tempList.Add(new ChronicPreview(r, PrescriptionType.ChronicRegister));
             }
+
+            foreach (var data in tempList.OrderByDescending(_ => _.AdjustDate))
+            {
+                Add(data);
+            }
+
         }
 
         public void GetReserveByCusId(int cusID) //取得預約慢箋
         {
             Clear();
+
+            List<ChronicPreview> tempList = new List<ChronicPreview>();
             var table = PrescriptionDb.GetReservePrescriptionByCusId(cusID);
             foreach (DataRow r in table.Rows)
             {
-                Add(new ChronicPreview(r, PrescriptionType.ChronicReserve));
+                tempList.Add(new ChronicPreview(r, PrescriptionType.ChronicReserve));
+            }
+
+            foreach (var data in tempList.OrderByDescending(_ => _.AdjustDate))
+            {
+                Add(data);
             }
         }
 
         public void GetNoCardByCusId(int cusID) //取得未過卡處方
         {
             Clear();
+
+            List<NoCardPreview> tempList = new List<NoCardPreview>();
+
             var table = PrescriptionDb.GetPrescriptionsNoGetCardByCusId(cusID);
             foreach (DataRow r in table.Rows)
             {
                 if (DateTimeExtensions.CountTimeDifferenceWithoutHoliday(DateTime.Today, r.Field<DateTime>("Adj_Date")) <= 10)
-                    Add(new NoCardPreview(r));
+                    tempList.Add(new NoCardPreview(r));
+            }
+
+            foreach (var data in tempList.OrderByDescending(_ => _.AdjustDate))
+            {
+                Add(data);
             }
         }
     }
