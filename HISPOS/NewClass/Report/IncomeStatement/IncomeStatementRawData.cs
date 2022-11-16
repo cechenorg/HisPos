fc1f4d3e689212e7b4d2f5da448695475bd8fcee
xilian <xilian@DESKTOP-38FCB7V>
2022-11-17 00:01:18 +0800

    Add GetIncomeStatementRawData

---
+++ HISPOS/NewClass/Report/IncomeStatement/IncomeStatementRawData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace His_Pos.NewClass.Report.IncomeStatement
+{
+    public class IncomeStatementDisplayData :ObservableObject
+    {
+        public string Name { get; set; }
+
+        public List<int> MonthlyValues { get; set;}
+
+        public ObservableCollection<IncomeStatementDisplayData> Childs { get; set; }
+    }
+
+    public class IncomeStatementRawData
+    {
+        public IncomeStatementRawData()
+        {
+
+        }
+        public string Name { get; set; }
+
+        public DateTime DT { get; set; }
+
+        public int Value { get; set; }
+
+        public string Note { get; set; }
+
+        public string Source { get; set; }
+    }
+}
