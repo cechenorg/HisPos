aaf23a25fcfc1c1416703cfb1dbb806b38a7a803
xilian <xilian@DESKTOP-38FCB7V>
2022-12-01 15:31:14 +0800

    add GetAccountCommonItems

---
+++ HISPOS/NewClass/Report/Accounts/AccountsCommonItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace His_Pos.NewClass.Report.Accounts
+{
+    public class AccountsCommonItem
+    {
+        public AccountsCommonItem(){}
+        public int acctLevel1 { get; set; }
+        public int acctLevel2 { get; set; }
+        public int acctLevel3 { get; set; }
+        public string AcctName { get; set; }
+    }
+}
