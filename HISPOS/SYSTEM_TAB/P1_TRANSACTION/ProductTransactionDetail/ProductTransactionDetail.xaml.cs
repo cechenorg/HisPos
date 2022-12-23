47d9493e182e23785effa0277aca1dd2b925a385
weilun593 <weilun593@gmail.com>
2022-12-23 11:58:41 +0800

    已刪除訂單，不能修改

--- HISPOS/SYSTEM_TAB/P1_TRANSACTION/ProductTransactionDetail/ProductTransactionDetail.xaml.cs
+++ HISPOS/SYSTEM_TAB/P1_TRANSACTION/ProductTransactionDetail/ProductTransactionDetail.xaml.cs
@@ -99,6 +99,18 @@ namespace His_Pos.SYSTEM_TAB.P1_TRANSACTION.ProductTransactionDetail
             {
                 btnReturn.IsEnabled = false;
             }
+            if(masterRow.Table.Columns.Contains("TraMas_IsEnable"))
+            {
+                int isEnable = Convert.ToInt32(masterRow["TraMas_IsEnable"]);
+                if(isEnable == 0)
+                {
+                    btnDelete.IsEnabled = false;
+                    btnReturn.IsEnabled = false;
+                    tbCardNum.IsEnabled = false;
+                    tbTaxNum.IsEnabled = false;
+                    tbCusName.IsEnabled = false;
+                }
+            }
         }
 
         private void GetEmployeeList()
