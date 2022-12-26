8b184108495b344d5b29dbcc46f66be371b317dd
weilun593 <weilun593@gmail.com>
2022-12-26 13:20:46 +0800

    M12補卡註記預設1正常即可

--- HISPOS/NewClass/Prescription/ICCard/Upload/IcDataUploadService2.cs
+++ HISPOS/NewClass/Prescription/ICCard/Upload/IcDataUploadService2.cs
@@ -148,8 +148,8 @@ namespace His_Pos.NewClass.Prescription.ICCard.Upload
                 OrTreatmentCode = string.IsNullOrEmpty(p.OrigTreatmentCode) ? "99999999999999999999" : p.OrigTreatmentCode;
                 CuOrgCode = p.Institution.ID;
 
-                if (makeUp || DateTime.Compare(((DateTime)p.AdjustDate).Date, DateTime.Now.Date) < 0)
-                    MakeUpMark = "2";
+                //if (makeUp || DateTime.Compare(((DateTime)p.AdjustDate).Date, DateTime.Now.Date) < 0)
+                //    MakeUpMark = "2";
                 MedicalPersonIcNumber = p.Pharmacist.IDNumber;
                 MainDiagnosisCode = p.MainDisease.ID;
                 if (!string.IsNullOrEmpty(p.SubDisease?.ID))
