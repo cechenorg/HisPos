432d1eee6422956b7d6206683dd1adead4553fb3
xilian <xilian@pixl.world>
2023-02-06 20:20:18 +0800

    Add HISPOSWebApiService
    (cherry picked from commit bbad5d0bd98cdcf7fc300db520be58aecda1fd07)

---
+++ HISPOS/Service/HISPOSWebApiService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace His_Pos.Service
+{
+    internal class HISPOSWebApiService
+    {
+
+        public bool SyncData()
+        {
+            WebClient webClient = new WebClient();
+
+            webClient.DownloadStringTaskAsync("");
+
+
+            return false;
+        }
+
+    }
+}
