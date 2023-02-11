8cd5f5a75750e42bf1c4bfd7d3ca026c13aa7052
xilian <xilian@pixl.world>
2023-02-11 11:13:27 +0800

    Add DTO netframework project
    (cherry picked from commit e3f412fdee86896ba9f0d512627c67df8a320232)

---
+++ DTO/WebService/DivisionDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace WebServiceDTO
+{
+    public class DivisionDTO
+    {
+        public string Div_ID { get; set; }
+        public string Div_Name { get; set; }
+    }
+}
---
+++ DTO/WebService/SingdeOTCDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServiceDTO
+{
+    public class SingdeOTCDTO
+    {
+        public string OTC_Code { get; set; }
+        public string OTC_Barcode { get; set; }
+        public string OTC_Name { get; set; }
+        public string OTC_Type { get; set; }
+        public int OTC_BagFact { get; set; }
+        public string OTC_StopUsing { get; set; }
+        public int OTC_InvQty { get; set; }
+        public string OTC_InvMemo { get; set; }
+        public int OTC_Price { get; set; }
+    }
+}
