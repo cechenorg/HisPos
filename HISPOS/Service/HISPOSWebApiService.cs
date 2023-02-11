a2d22f7192e58b08f36e8dda2d6f5e3e55f2bb09
xilian <xilian@pixl.world>
2023-02-11 11:08:10 +0800

    test
    (cherry picked from commit 78fc8c2230c63b78e092cab731386c941ddfce5b)

--- HISPOS/Service/HISPOSWebApiService.cs
+++ HISPOS/Service/HISPOSWebApiService.cs
@@ -1,23 +1,49 @@
-﻿using System;
+﻿using Microsoft.Reporting.Map.WebForms.BingMaps;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Google.Protobuf;
+using Newtonsoft.Json;
 
 namespace His_Pos.Service
 {
     internal class HISPOSWebApiService
     {
 
-        public bool SyncData()
+        private const string webapiPath = @"https://kaokaodepon.singde.com.tw:60005/ServerAPI/";
+        public async Task SyncData()
         {
-            WebClient webClient = new WebClient();
 
-            webClient.DownloadStringTaskAsync("");
+            //await GetAPIData<AdjustCaseDTO>("GetAdjustCase");
+        }
 
+        private async Task<T> GetAPIData<T>(string route)
+        {
+            string targetUrl = webapiPath + "GetAdjustCase";
 
-            return false;
+            HttpWebRequest request = WebRequest.Create(targetUrl) as HttpWebRequest;
+            request.Method = "GET";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.Timeout = 30000;
+
+            string result = "";
+            // 取得回應資料
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            {
+                using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+                {
+                    result = sr.ReadToEnd();
+                }
+            }
+
+            
+            var DTO = JsonConvert.DeserializeObject<T>(result);
+
+            return DTO;
         }
 
     }
