d30e50e9030e99bb47bec1c6f7c2be5e084d1564
xilian <xilian@pixl.world>
2023-02-28 00:56:54 +0800

    Add PharmacyDTO
    (cherry picked from commit f5f29bf692542c30c5324f376c87003df0f9f8fb)

---
+++ DTO/WebService/PharmacyDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.WebService
+{
+    public class PharmacyDTO
+    {
+
+        public PharmacyDTO(){}
+        public string PHAMAS_ID { get; set; }
+        public string PHAMAS_NAME { get; set; }
+        public string PHAMAS_MEDICALNUM { get; set; }
+        public string PHAMAS_TEL { get; set; }
+        public string PHAMAS_ADDR { get; set; }
+        public DateTime PHAMAS_VALIDUSEDATE { get; set; }
+        public string PHAMAS_VerifyKey { get; set; }
+        public string PHAMAS_DbTarget { get; set; }
+        public string PHAMAS_GroupServer { get; set; }
+        public string PHAMAS_TAXNUM { get; set; }
+        public string PHAMAS_GUID { get; set; }
+
+    }
+}
